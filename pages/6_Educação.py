5aa727f89ecdc8df3808bb7af675ed9e42694a73
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-07-01 05:11:00 -0300

    Update 6_Educação.py

--- pages/6_Educação.py
+++ pages/6_Educação.py
@@ -1,1 +1,6 @@
-st.subheader("Em breve...")
+import streamlit as st
+from streamlit_extras.colored_header import colored_header
+
+    colored_header(label="Indicadores de Segurança: em breve ⌛",
+                   description="Percentual de domicílios com bens duráveis no Paraná",
+                   color_name="violet-70",)
