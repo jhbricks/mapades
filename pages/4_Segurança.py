bd16f336a5c0aa671c895163070711b5e6558f4b
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-07-01 05:13:18 -0300

    Update 4_Segurança.py

--- pages/4_Segurança.py
+++ pages/4_Segurança.py
@@ -2,5 +2,4 @@ import streamlit as st
 from streamlit_extras.colored_header import colored_header
 
     colored_header(label="Indicadores de Segurança: em breve ⌛",
-                   description="Percentual de domicílios com bens duráveis no Paraná",
                    color_name="orange-70",)
