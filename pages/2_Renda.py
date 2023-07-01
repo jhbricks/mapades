16fae1102940c978f0aa6850fbcc725e7e70a708
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-07-01 03:51:16 -0300

    Update 2_Renda.py

--- pages/2_Renda.py
+++ pages/2_Renda.py
@@ -1,1 +1,65 @@
-st.subheader("AAAAAAAAAA")
+import streamlit as st
+from streamlit_extras.colored_header import colored_header
+
+st.set_page_config(layout="wide")
+
+#Selecionar a área [Radio horizontal]
+st.markdown("<h3><font size='8'  color='red'>Renda</font></font></h3>", unsafe_allow_html=True)
+area = st.radio("Selecione uma área:",("Paraná","Núcleo Territorial Central"))
+st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
+
+
+if area == "Paraná":
+  st.header("Renda do Paraná")
+  t1, t2, t3, t4 = st.tabs(["Coeficiente de Gini", "Renda da população", "Renda dos declarantes do IRPF", "Renda da população feminina"])
+  with t1:
+    colored_header(label="Coeficiente de Gini",
+                   description="2.1	Coeficiente de Gini renda domiciliar per capita ",
+                   color_name="red-70",)
+
+    
+  with t2:
+    colored_header(label="Renda média da população",
+                   description="Renda média da população",
+                   color_name="red-70",)
+
+
+  with t3:
+    colored_header(label="Renda dos declarantes do IRPF",
+                   description="Renda média dos declarentes do IRPF",
+                   color_name="red-70",)
+
+
+  with t4:
+    colored_header(label="População feminina",
+                   description="Percentagem da população feminina no Paraná",
+                   color_name="red-70",)
+
+
+
+
+if area == "Núcleo Territorial Central":
+  st.header("Renda do Núcleo Territorial Central")
+  t1, t2, t3, t4 = st.tabs(["Coeficiente de Gini", "Renda da população", "Renda dos declarantes do IRPF", "Renda da população feminina"])
+  with t1:
+    colored_header(label="Coeficiente de Gini",
+                   description="2.1	Coeficiente de Gini renda domiciliar per capita ",
+                   color_name="red-70",)
+
+    
+  with t2:
+    colored_header(label="Renda média da população",
+                   description="Renda média da população",
+                   color_name="red-70",)
+
+
+  with t3:
+    colored_header(label="Renda dos declarantes do IRPF",
+                   description="Renda média dos declarentes do IRPF",
+                   color_name="red-70",)
+
+
+  with t4:
+    colored_header(label="População feminina",
+                   description="Percentagem da população feminina no Paraná",
+                   color_name="red-70",)
