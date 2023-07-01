2f6459d000294fe1d20da1d582ada44486b0b3eb
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-07-01 05:28:15 -0300

    Update 4_Segurança.py

--- pages/4_Segurança.py
+++ pages/4_Segurança.py
@@ -7,5 +7,5 @@ colored_header(label="Indicadores de Segurança: em breve ⌛", description="   
 label = "Nice title"
 color_name = "red-70"
 st.subheader(label)
-st.write(f'<hr style="background-color: {color(color_name)}; margin-top: 0;''margin-bottom: 0; height: 3px; border: none; border-radius: 3px;">',
+st.write(f'<hr style="background-color: (color_name); margin-top: 0;''margin-bottom: 0; height: 3px; border: none; border-radius: 3px;">',
         unsafe_allow_html=True,)
