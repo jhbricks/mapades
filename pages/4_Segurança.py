e5fd1d519ba13bed65e8cc5af113f095747e05ad
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-07-01 05:27:34 -0300

    Update 4_Segurança.py

--- pages/4_Segurança.py
+++ pages/4_Segurança.py
@@ -2,3 +2,10 @@ import streamlit as st
 from streamlit_extras.colored_header import colored_header
 
 colored_header(label="Indicadores de Segurança: em breve ⌛", description="   ", color_name="orange-70",)
+
+
+label = "Nice title"
+color_name = "red-70"
+st.subheader(label)
+st.write(f'<hr style="background-color: {color(color_name)}; margin-top: 0;''margin-bottom: 0; height: 3px; border: none; border-radius: 3px;">',
+        unsafe_allow_html=True,)
