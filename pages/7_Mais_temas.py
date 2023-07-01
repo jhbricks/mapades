91a0753d77b21d7be873f838df0de98822a79745
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-07-01 05:33:21 -0300

    Update 7_Mais_temas.py

--- pages/7_Mais_temas.py
+++ pages/7_Mais_temas.py
@@ -2,4 +2,5 @@ import streamlit as st
 from streamlit_extras.colored_header import colored_header
 
     colored_header(label="Mais indicadores em breve ⌛",
+                   description="   ",
                    color_name="yellow-80",)
