75eff02b8b28a405bb18f1f8275ab51992a9f5eb
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-07-01 05:33:10 -0300

    Update 6_Educação.py

--- pages/6_Educação.py
+++ pages/6_Educação.py
@@ -2,4 +2,5 @@ import streamlit as st
 from streamlit_extras.colored_header import colored_header
 
     colored_header(label="Indicadores de Educação: em breve ⌛",
+                   description="   ",
                    color_name="violet-70",)
