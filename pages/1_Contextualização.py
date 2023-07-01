ce0d972a701352f2ed983178bacfe3f9689e5d82
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-07-01 02:47:46 -0300

    Update 1_Contextualização.py

--- pages/1_Contextualização.py
+++ pages/1_Contextualização.py
@@ -7,7 +7,7 @@ st.markdown("<h3><font size='8'  color='red'>Contextualização</font></font></h
 st.write('<style>div.row-widget.stRadio > div{flex-direction:row;justify-content: center;} </style>', unsafe_allow_html=True)
 st.write('<style>div.st-bf{flex-direction:column;} div.st-ag{font-weight:bold;padding-left:2px;}</style>', unsafe_allow_html=True)
 
-area = st.radio("Selecione uma área:",("Paraná","Núcleo Territorial Central")
+area = st.radio("Selecione uma área:",("Paraná","Núcleo Territorial Central"))
 
 if area == "Paraná":
   t1, t2, t3, t4, t5, t6 = st.tabs(["População residente", "População feminina", "População preta/parda", "Densidade demográfica", "Grau de urbanização", "Razão de dependência"])
