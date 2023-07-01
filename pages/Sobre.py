77b71e9d83d070a5a6d542ad6d97fdff7b29e16a
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-06-30 23:13:38 -0300

    Create Sobre.py

---
+++ pages/Sobre.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from streamlit_extras.colored_header import colored_header
+
+colored_header(
+    label="Sobre",
+    description="This is a description",
+    color_name="green-70",
+)
+
+st.subheader("Realizadores")
