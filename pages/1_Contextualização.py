cdb38eac87f898b7776956fdcece5a470e2977e8
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-07-01 03:11:38 -0300

    Update 1_Contextualização.py

--- pages/1_Contextualização.py
+++ pages/1_Contextualização.py
@@ -5,16 +5,6 @@ st.markdown("<h3><font size='8'  color='red'>Contextualização</font></font></h
 #area = st.selectbox("Selecione uma área:", ("Paraná", "Núcleo Territorial Central
 area = st.radio("Selecione uma área:",("Paraná","Núcleo Territorial Central"))
 
-st.markdown(
-    """
-    <style>
-    div.row-widget.stempty > div{flex-direction:row;justify-content: center;}
-    div.st-bf{flex-direction:column;} div.st-ag{font-weight:bold;padding-left:2px;}
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
 if area == "Paraná":
   t1, t2, t3, t4, t5, t6 = st.tabs(["População residente", "População feminina", "População preta/parda", "Densidade demográfica", "Grau de urbanização", "Razão de dependência"])
   with t1:
