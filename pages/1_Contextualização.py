6ff7b8d5f40f1475d59c1c50531e0f47bac8a26f
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-07-01 01:41:55 -0300

    Update 1_Contextualização.py

--- pages/1_Contextualização.py
+++ pages/1_Contextualização.py
@@ -9,13 +9,25 @@ st.markdown(
 area = st.selectbox("Selecione uma área:", ("Paraná", "Núcleo Territorial Central"))
 
 if area == "Paraná":
-  t1, t2, t3, t4, t5 = st.tabs(["População residente", "População feminina", "População preta/parda", "Densidade demográfica", "Grau de urbanização", "Razão de dependência"])
+  t1, t2, t3, t4, t5, t6 = st.tabs(["População residente", "População feminina", "População preta/parda", "Densidade demográfica", "Grau de urbanização", "Razão de dependência"])
   with t1:
     st.subheader(":red[População residente]")
     st.caption("População total do Paraná")
   with t2:
     st.subheader(":red[População feminina]")
     st.caption("Percentagem da população feminina no Paraná")
+  with t3:
+    label= "Nice title",
+    description="Cool description",
+    color_name = "red-70")
+    st.subheader(label)
+    st.write(
+        f'<hr style="background-color: {color(color_name)}; margin-top: 0;'
+        ' margin-bottom: 0; height: 3px; border: none; border-radius: 3px;">',
+        unsafe_allow_html=True,
+    )
+    if description:
+        st.caption(description)
     
     
 
