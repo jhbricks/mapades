e11f03de07c8d38fcebb67148dd5edd6cba59933
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-07-01 05:36:50 -0300

    Update 9_Ajuda.py

--- pages/9_Ajuda.py
+++ pages/9_Ajuda.py
@@ -1,1 +1,9 @@
-st.subheader("AAAAAAAAAA")
+import streamlit as st
+from streamlit_extras.colored_header import colored_header
+
+colored_header(
+    label="Como ler mapas",
+    description="   ",
+    color_name="light-blue-70",
+)
+st.markdown("""texto texto""")
