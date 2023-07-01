b0f3afde508e3b9ce8cc4ab605bc225d84e69c90
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-07-01 05:09:09 -0300

    Update 5_Meio_ambiente.py

--- pages/5_Meio_ambiente.py
+++ pages/5_Meio_ambiente.py
@@ -1,1 +1,7 @@
-st.subheader("Em breve...")
+import streamlit as st
+from streamlit_extras.colored_header import colored_header
+
+    colored_header(label="Indicadores de Meio ambiente: em breve ⌛",
+                   description="Percentual de domicílios com bens duráveis no Paraná",
+                   color_name="green-70",)
+
