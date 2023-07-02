1d52ae52ebba16f7448d2b202bd35d5c5f3ba515
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-07-02 08:12:55 -0300

    Update 4_Segurança.py

--- pages/4_Segurança.py
+++ pages/4_Segurança.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit_extras.colored_header import colored_header
+from teste.teste import mapa
 
-colored_header(label="Indicadores de Segurança: em breve ⌛", description="   ", color_name="orange-70",)
+colored_header(label=":orange[Indicadores de Segurança: em breve ⌛]", description="   ", color_name="orange-70",)
 
-
-
+mapa('PR', 'pop', 'População', 'FisherJenks', 5, 'Oranges', ['Municípios','População'], 'AAA')
