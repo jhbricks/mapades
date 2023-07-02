7fa90dbefbf5859d739d5ef50d7608190f8ecd3c
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-07-02 03:24:40 -0300

    Update 2_Renda.py

--- pages/2_Renda.py
+++ pages/2_Renda.py
@@ -162,14 +162,14 @@ if area == "Paraná":
                         m.to_streamlit()
 
 # Lista com os argumentos específicos para cada mapa
-    mapas = (ren_PR, 'Coeficiente de Gini', 'FisherJenks', 3, 'RdPu', ['Município', 'Coeficiente de Gini'], 'Coeficiente de Gini da Renda Domiciliar per Capita')
+    mapa = (ren_PR, 'Coeficiente de Gini', 'FisherJenks', 3, 'RdPu', ['Município', 'Coeficiente de Gini'], 'Coeficiente de Gini da Renda Domiciliar per Capita')
     construir_mapa(mapa)
 
   with t4:
     colored_header(label="Renda dos declarantes do IRPF",
                    description="Renda média dos declarentes do IRPF no Paraná",
                    color_name="red-70",)
-    mapas = (ren_PR, 'Coeficiente de Gini', 'FisherJenks', 3, 'RdPu', ['Município', 'Coeficiente de Gini'], 'Coeficiente de Gini da Renda Domiciliar per Capita')
+    mapa = (ren_PR, 'Coeficiente de Gini', 'FisherJenks', 3, 'RdPu', ['Município', 'Coeficiente de Gini'], 'Coeficiente de Gini da Renda Domiciliar per Capita')
 
 
     construir_mapa(mapa)
