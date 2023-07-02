7a94c4ecc990ffc2a233d6b9bc7b827861cf1572
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-07-02 02:30:07 -0300

    Update 2_Renda.py

--- pages/2_Renda.py
+++ pages/2_Renda.py
@@ -128,7 +128,7 @@ if area == "Paraná":
       st.markdown("<h3><font size='+5'> Municípios com o <font size='+5' color='green'>maior</font> e <font size='+5' color='red'>menor</font> <font size='+5'> valor:</font></h3>", unsafe_allow_html=True)
       st.markdown(f"<p style='line-height: 0.7;'><font size='+10' color='green'>{arrow_u}</font> <font size='+5'>{max_str} = {ind_mx}</font></p>", unsafe_allow_html=True)
       st.markdown(f"<p style='line-height: 0.5;'><font size='+10' color='red'>{arrow_d}</font> <font size='+5'>{min_str} = {ind_mn}</font></p>", unsafe_allow_html=True)
-      st.markdown("<h3><font size='+5'> Média do Coeficiente de Gini no Paraná:, unsafe_allow_html= True)
+      st.markdown("<h3><font size='+5'> Média do Coeficiente de Gini no Paraná:</font></h3>", unsafe_allow_html= True)
       st.markdown(f"<p style='font-weight: bold; font-size: 1.7em;'>{media}</p>", unsafe_allow_html=True)
 
   
