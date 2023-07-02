c43d770fe1eeb00c3ffc43170c19f6056a765e15
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-07-02 07:56:07 -0300

    Update 1_Contextualização.py

--- pages/1_Contextualização.py
+++ pages/1_Contextualização.py
@@ -121,7 +121,7 @@ if area == "Paraná":
 
     m = mapa('PR', pop, 'População', 'FisherJenks', 7, 'YlGnBu', ['Município','População'],'População residente do Paraná','inteiro', 'hab')
     
-    folium_static(m)
+    m.to_streamlit()
   
   with t3:
     colored_header(label="Grau de urbanização",
