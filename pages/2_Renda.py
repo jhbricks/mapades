aca0e8ca11a6e96746725e9867f2d3937e28d271
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-07-01 22:22:16 -0300

    Update 2_Renda.py

--- pages/2_Renda.py
+++ pages/2_Renda.py
@@ -65,7 +65,7 @@ if area == "Paraná":
       fields= ['Município','Coeficiente de Gini'],
       legend_title='Índice de Gini da Renda Domiciliar per Capita',
       legend_position = "bottomright",
-      layer_name = "'Índice de Gini da Renda Domiciliar per Capita (2010)",
+      layer_name = "'Índice de Gini da Renda Domiciliar per Capita",
       style={"stroke": True, "color": "#000000", "weight": 1, "fillOpacity": 1}
     )
     m.to_streamlit()
