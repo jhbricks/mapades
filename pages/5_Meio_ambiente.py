79fb24917724f529a0878c42d1f34c64f3cb5f9b
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-07-03 03:43:38 -0300

    Update 5_Meio_ambiente.py

--- pages/5_Meio_ambiente.py
+++ pages/5_Meio_ambiente.py
@@ -1,7 +1,13 @@
 import streamlit as st
 from streamlit_extras.colored_header import colored_header
 
-    colored_header(label="Indicadores de Meio ambiente: em breve ⌛",
-                   description="   ",
-                   color_name="green-70",)
+import awesome_streamlit as ast
+
+ast.experiments.write_hello_world()
+
+colored_header(label="Indicadores de Meio ambiente: em breve ⌛",
+               description="   ",
+               color_name="green-70",)
+
+
 
