5690faeae7ac2ba9851a19d5d3ca3d3470265827
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-07-05 19:28:41 -0300

    Update calculos.py

--- deff/calculos.py
+++ deff/calculos.py
@@ -86,25 +86,25 @@ def conta (area,arq,ind,ano,calc=None,tipo=None,unidade=None):
         st.markdown(f"<h3><font style='font-weight: bold;><font size:'+5'> {DEM} </font> habitantes por km²</font></h3>", unsafe_allow_html=True)
     elif ind == 'Grau de Urbanização (%)':
         somaU = data['PCU'].sum()
-        TU = (somaU*100)/somaT
+        TU = ((somaU*100)/somaT).round(2)
         st.markdown(f"<h3><font size='+5'> Grau de Urbanização no {nome} em {ano}:</font></h3>", unsafe_allow_html=True)
         st.markdown(f"<h3><font style='font-weight: bold;><font size:'+5'> {TU} </font> %</font></h3>", unsafe_allow_html=True)
     elif ind == 'População feminina (%)':
         somaF = data['PCF'].sum()
-        TF = (somaF*100)/somaT
+        TF = ((somaF*100)/somaT).round(2)
         st.markdown(f"<h3><font size='+5'> População feminina total no {nome} em {ano}:</font></h3>", unsafe_allow_html=True)
         st.markdown(f"<h3><font style='font-weight: bold;><font size:'+5'> {TF} </font> %</font></h3>", unsafe_allow_html=True)
     elif ind == 'População preta ou parda (%)':
         somaPR=data['PCPR'].sum()
         somaPA=data['PCPA'].sum()
-        TPP= ((somaPR + somaPA)*100)/somaT
+        TPP= (((somaPR + somaPA)*100)/somaT).round(2)
         st.markdown(f"<h3><font size='+5'> População preta ou parda total no {nome} em {ano}:</font></h3>", unsafe_allow_html=True)
         st.markdown(f"<h3><font style='font-weight: bold;><font size:'+5'> {TPP} </font> %</font></h3>", unsafe_allow_html=True)
     elif ind == 'Razão de Dependência (%)':
         soma65 = data['PP65'].sum()
         soma14 = data['PP14'].sum()
         somaPT = data['PPT'].sum()
-        RD = ((soma65 + soma14)*100)/somaPT
+        RD = (((soma65 + soma14)*100)/somaPT).round(2)
         st.markdown(f"<h3><font size='+5'> Razão de dependência no {nome} em {ano}:</font></h3>", unsafe_allow_html=True)
         st.markdown(f"<h3><font style='font-weight: bold;><font size:'+5'> {RD} </font> %</font></h3>", unsafe_allow_html=True)
     else:
@@ -140,7 +140,7 @@ def conta_renda (area,arq,ind,ano,calc=None,tipo=None,unidade=None):
     if ind == "Rendimento médio da população feminina/masculina (%)":
         somaF = data['RMF'].sum()
         somaM = data['RMM'].sum()
-        RFM = (somaF*100)/somaM
+        RFM = ((somaF*100)/somaM).round(2)
         st.markdown(f"<h3><font size='+5'> Percentual do Rendimento médio mensal da população feminina em relação a masculina no {nome} em {ano}:</font></h3>", unsafe_allow_html=True)
         st.markdown(f"<h3><font style='font-weight: bold;><font size:'+5'> {RFM} % </font> %</font></h3>", unsafe_allow_html=True)
     elif ind == 'Renda Média da População (R$ mil)':
