65d8013167a9f0fd3ab8dcea42be9bb5da56dbb1
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-07-05 20:36:13 -0300

    Update 4_Segurança.py

--- pages/4_Segurança.py
+++ pages/4_Segurança.py
@@ -1,15 +1,4 @@
 import streamlit as st
 from streamlit_extras.colored_header import colored_header
-from teste.teste import mapa
-from streamlit_folium import folium_static
-import folium
-import geopandas as gpd
-import leafmap.foliumap as leafmap
-import pandas as pd
-
-contexto = "./dados/csv/contexto.csv"
-pop = "./dados/csv/pop_2021.csv"
-renda = "./dados/csv/renda.csv"
-riqueza = "./dados/csv/riqueza.csv"
 
 colored_header(label=":orange[Indicadores de Segurança: em breve ⌛]", description="   ", color_name="orange-70",)
