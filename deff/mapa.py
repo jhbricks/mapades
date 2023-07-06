f7c92d246a3bf09904aab55fe2f0d2056503a454
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-07-06 05:19:22 -0300

    Update mapa.py

--- deff/mapa.py
+++ deff/mapa.py
@@ -74,23 +74,6 @@ def mapa (area,arq,ind,scheme,k,cmap,fields,title):
              legend_position='Bottomright',
              layer_name=title,
              )
-  style_function = lambda x: {'fillColor': '#ffffff',
-                       	      'color': '#000000',
-                              'fillOpacity': 0,
-                              'weight': 0.1}
-  highlight_function = lambda x: {'fillColor': '#000000',
-				  'color': '#000000',
-                                  'fillOpacity': 1,
-                                  'weight': 0.1}
-  NIL = folium.features.GeoJson(data,
-				style_function=style_function,
-           			control=False,
-           			highlight_function=highlight_function,
-           			tooltip=folium.features.GeoJsonTooltip(fields=fields, aliases=fields,
-								       style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 10px;")))
-  m.add_child(NIL)
-  m.keep_in_front(NIL)	
-
 ########VALORES DE MX E MN DAS VARIAVEIS
   max_value = data[ind].max()
   min_value = data[ind].min()
