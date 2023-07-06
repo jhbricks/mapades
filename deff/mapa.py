cf83507006ca51a85e471c9be653ca85bc055a4f
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-07-06 06:04:56 -0300

    Update mapa.py

--- deff/mapa.py
+++ deff/mapa.py
@@ -23,7 +23,7 @@ riqueza = "./dados/csv/riqueza.csv"
 #fields = variaveis que aparecem no popup, ex: ['Município','População']
 #title = título do mapa e da legenda
 
-def mapa (z,area,arq,ind,scheme,k,cmap,fields,title):
+def mapa (z, zmn, zmx,area,arq,ind,scheme,k,cmap,fields,title):
 ######encaminha o geojson da area
   if area == 'PR':
     arq_g = "./dados/geojson/PR.geojson"
@@ -54,11 +54,11 @@ def mapa (z,area,arq,ind,scheme,k,cmap,fields,title):
     print("O arquivo não é um GeoDataFrame")
     exit()
 ##########################MAPA
-#######ZOOM TO LAYER
-
 ########MAPA INICIAL
   m = leafmap.Map(center=[lat,lon],
 		  zoom = z,
+		  zoom_min = zmn,
+		  zoom_max = zmx,
                   draw_control=False,
                   measure_control=False,
                   fullscreen_control=False,
