95d8ccf4588e4cff5e76a7e95bc868e4bb95a847
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-07-06 04:53:09 -0300

    Update mapa.py

--- deff/mapa.py
+++ deff/mapa.py
@@ -64,7 +64,7 @@ def mapa (area,arq,ind,scheme,k,cmap,fields,title):
                   attribution_control=True)
   
 #######ADICIONAR O MERGE GDF
-  style = {"stroke": True, "color": "#000000", "weight": 0.1, "fillOpacity": 0}
+  style = {"stroke": True, "color": "#000000"}
 	
   m.add_data(data = data,
              column=ind,
