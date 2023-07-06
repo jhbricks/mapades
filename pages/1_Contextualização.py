464f8bb63fa8f67ba16fe43fde05792ddafa1551
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-07-06 02:45:08 -0300

    Update 1_Contextualização.py

--- pages/1_Contextualização.py
+++ pages/1_Contextualização.py
@@ -2,12 +2,13 @@ import streamlit as st
 from streamlit_extras.colored_header import colored_header
 from streamlit_folium import folium_static
 from deff.mapa import mapa
-from deff.mapa import mx_mn
+from deff.map import mx_mn
 from deff.calculos import conta
 import folium
 import geopandas as gpd
 import leafmap.foliumap as leafmap
 import pandas as pd
+import numpy as np
 
 st.set_page_config(layout="wide")
 
