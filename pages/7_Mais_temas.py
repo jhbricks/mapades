7e670ef911933aee91bd1813874cdb803664bcb1
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-09-11 18:39:09 -0300

    Update 7_Mais_temas.py

--- pages/7_Mais_temas.py
+++ pages/7_Mais_temas.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from streamlit_extras.colored_header import colored_header
 
+st.set_page_config(layout="wide", page_title="Mais temas - Mapa da Desigualdade")
+
 colored_header(label=":yellow[Mais indicadores em breve ⌛]",
                description="   ",
                color_name="yellow-80",)
