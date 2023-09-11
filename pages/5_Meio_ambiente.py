947e3462bed4feeee50ecbdc1414532d3e54e546
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-09-11 18:38:34 -0300

    Update 5_Meio_ambiente.py

--- pages/5_Meio_ambiente.py
+++ pages/5_Meio_ambiente.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from streamlit_extras.colored_header import colored_header
 
+st.set_page_config(layout="wide", page_title="Meio Ambiente - Mapa da Desigualdade")
+
 colored_header(label=":green[Indicadores de Meio Ambiente: em breve ⌛]",
                description="   ",
                color_name="green-70",)
