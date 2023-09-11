fdfc4aeaf667fdf3b47e331832b11ae5e82047fd
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-09-11 18:38:52 -0300

    Update 6_Educação.py

--- pages/6_Educação.py
+++ pages/6_Educação.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from streamlit_extras.colored_header import colored_header
 
+st.set_page_config(layout="wide", page_title="Educação - Mapa da Desigualdade")
+
 colored_header(label=":violet[Indicadores de Educação: em breve ⌛]",
                description="   ",
                color_name="violet-70",)
