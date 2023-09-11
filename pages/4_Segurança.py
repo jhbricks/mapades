8d6fdeee864869464e380e818bf284acbde08f3c
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-09-11 18:38:12 -0300

    Update 4_Segurança.py

--- pages/4_Segurança.py
+++ pages/4_Segurança.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from streamlit_extras.colored_header import colored_header
 
+st.set_page_config(layout="wide", page_title="Segurança - Mapa da Desigualdade")
+
 colored_header(label=":orange[Indicadores de Segurança: em breve ⌛]", description="   ", color_name="orange-70",)
 
