6a6f38f50e188bca2a3b4eb992a3795adf8fbe52
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-09-11 21:16:12 -0300

    a

--- deff/mapa__.py
+++ deff/mapa__.py
@@ -86,23 +86,22 @@ def mapa1 (area,arq,ind,scheme,k,cmap,fields,title):
 	     zoom_to_layer=True,
              style=style,
              hover_style=hover_style)
-  data = gdf
 ########VALORES DE MX E MN DAS VARIAVEIS
-  max_value = data[ind].max()
-  min_value = data[ind].min()
-  max_municipio = data.loc[data[ind] == max_value, "Município"].iloc[0]
-  min_municipio = data.loc[data[ind] == min_value, "Município"].iloc[0]
+#  max_value = data[ind].max()
+#  min_value = data[ind].min()
+#  max_municipio = data.loc[data[ind] == max_value, "Município"].iloc[0]
+#  min_municipio = data.loc[data[ind] == min_value, "Município"].iloc[0]
 #####ADICIONAR MX E MN NO MAPA
-  folium.Marker([data.loc[data[ind] == max_value, "Y"].iloc[0],
-                 data.loc[data[ind] == max_value, "X"].iloc[0]],
-                popup=f"Maior valor: {max_value}<br>{max_municipio}",
-                icon=folium.Icon(color="darkpurple", icon="arrow-up"),
-               ).add_to(m) 
-  folium.Marker([data.loc[data[ind] == min_value, "Y"].iloc[0],
-	         data.loc[data[ind] == min_value, "X"].iloc[0]],
-                popup=f"Menor valor: {min_value}<br>{min_municipio}",
-                icon=folium.Icon(color="purple", icon="arrow-down"),
-               ).add_to(m)
+#  folium.Marker([data.loc[data[ind] == max_value, "Y"].iloc[0],
+#                 data.loc[data[ind] == max_value, "X"].iloc[0]],
+#                popup=f"Maior valor: {max_value}<br>{max_municipio}",
+#                icon=folium.Icon(color="darkpurple", icon="arrow-up"),
+#               ).add_to(m) 
+#  folium.Marker([data.loc[data[ind] == min_value, "Y"].iloc[0],
+#	         data.loc[data[ind] == min_value, "X"].iloc[0]],
+#                popup=f"Menor valor: {min_value}<br>{min_municipio}",
+#                icon=folium.Icon(color="purple", icon="arrow-down"),
+#               ).add_to(m)
 #########ADICIONAR NO STREAMLIT
  # m.zoom_to_bounds()
 
