f17848dae885d6d07f9112fbd29d2cfe88e72147
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-09-11 21:30:07 -0300

    a

--- deff/mapa__.py
+++ deff/mapa__.py
@@ -115,8 +115,8 @@ def mapa1 (area,arq,ind,scheme,k,cmap,fields,title):
  
 
   #components = [[-26.80, -54.67], [-22.44,-47.98]]
-  st_map_bounds(m, [[-26.80, -54.67], [-22.44,-47.98]])
-  
+  #st_map_bounds(m, [[-26.80, -54.67], [-22.44,-47.98]])
+  zoom_to_gdf(m, gdf)
   m.to_streamlit()
 
 
