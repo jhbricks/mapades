a62305652fac0e52364789158bc8397a0122fce3
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-09-12 21:55:57 -0300

    a

--- pages/3_Riqueza.py
+++ pages/3_Riqueza.py
@@ -81,7 +81,7 @@ if area == "Paraná":
       st.markdown("**Indica o número de veículos automotores por pessoa disponibilizado pelo Instituto Paranaense de Desenvolvimento Econômico e Social (Ipardes) para o ano de 2021**") 
       conta ('PR',riqueza,'Veículos por pessoa',2021,
              'Número de veículos por pessoas','media', 'veículo/hab')
-      grafico ('PR',riqueza,'Veículos por pessoa')
+      grafico ('PR',riqueza,'Veículos por pessoa','Veículo/hab')
 
     
   with t3:
