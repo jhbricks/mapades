dd6557ed7106c8da5460ec2a607b387035550845
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-09-13 10:36:14 -0300

    a

--- pages/8_Sobre.py
+++ pages/8_Sobre.py
@@ -1,6 +1,17 @@
 import streamlit as st
 from streamlit_extras.colored_header import colored_header
 from PIL import Image
+
+st.markdown("""
+        <style>
+               .block-container {
+                    padding-top: 1rem;
+                    padding-bottom: 0rem;
+                    padding-left: 5rem;
+                    padding-right: 5rem;
+                }
+        </style>
+        """, unsafe_allow_html=True)
 st.set_page_config(layout="wide", page_title="Sobre")
 
 colored_header(
@@ -29,13 +40,15 @@ colored_header(
     color_name="blue-green-70",
 )
 km = "./dados/imagem/km.png"
-ge = "./dados/imagem/gemaeco.png"
+ge = "./dados/imagem/ufprge.png"
 i1 = Image.open(km)
 i2 = Image.open(ge)
+a1 = i1.resize((600, 400))
+a2 = i2.resize((600,400))
 c1, c2 = st.columns(2)
 
 with c1:
-    st.image(i1)
+    st.image(a1)
     st.markdown("""A **Kurytiba Metropole** é uma associação sem fins
     lucrativos e apartidária que tem como objetivo
     transformar Curitiba em uma metrópole
@@ -46,7 +59,7 @@ with c1:
     """)
 
 with c2:
-    st.image(i2)
+    st.image(a2)
     st.markdown("""O **Grupo de Estudos em MacroEconomia Ecológica
     (GEMAECO)** é um grupo de pesquisa vinculado à
     Universidade Federal do Paraná (UFPR),
