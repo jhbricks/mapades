802aa4aec628eef9d52783023ec4e41fb822b90d
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-09-13 10:42:21 -0300

    a

--- pages/8_Sobre.py
+++ pages/8_Sobre.py
@@ -33,8 +33,8 @@ km = "./dados/imagem/km.png"
 ge = "./dados/imagem/ufprge.png"
 i1 = Image.open(km)
 i2 = Image.open(ge)
-a1 = i1.resize((200, 110))
-a2 = i2.resize((200,95))
+a1 = i1.resize((210, 100))
+a2 = i2.resize((200,100))
 c1, c2 = st.columns(2)
 
 with c1:
