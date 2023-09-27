af166f7eb399f93d286bb37b6faf5fbcb761fa12
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-09-27 17:24:53 -0300

    teste

--- pages/Classificação.py
+++ pages/Classificação.py
@@ -3,8 +3,7 @@ import pandas as pd
 import geopandas as gpd
 from streamlit_extras.colored_header import colored_header
 
-st.set_page_config(layout="wide")
-colored_header(label="Sobre",description="   ",color_name="blue-green-70",)
+st.set_page_config(layout="wide",page_title='Classificação dos dados')
 st.markdown("""
         <style>
                .block-container {
@@ -23,47 +22,19 @@ st.markdown(""" **Classificação de dados** explicação
 """)
 
 #inserir os arquivos csv e geojson
-option = st.selectbox('Escolha os arquivos que irá adicionar:',('CSV', 'GeoJSON e CSV', 'GeoDataFrame (gdf)'))
-if option == 'GeoJSON E CSV':
-    arq = st.file_uploader("Carregue os arquivos", type={"csv", "geojson"},accept_multiple_files=True)
-    if arq is not None:
-        csv_arq = None
-        geo_arq = None
-        for arquivo in arq:
-            if arquivo.type == 'application/vnd.geo+json':
-                geo_arq = arquivo
-            elif arquivo.type == 'text/csv':
-                csv_arq = arquivo
-    # Verifica se ambos os arquivos foram carregados
-    if csv_arq is not None and geo_arq is not None:
-        arq_gpd = gpd.read_file(geo_arq)
-        arq_pd = pd.read_csv(csv_arq)
-        data = arq_gpd.merge(arq_pd, on="Município")
-    else:
-        st.warning("Por favor, carregue um arquivo CSV e um arquivo GeoJSON.")
-elif option == 'CSV':
-    arq = st.file_uploader("Carregue os arquivos", type={"csv"})
-    if arq is not None:
-        csv_arq = None
-        for arquivo in arq:
-            arquivo.type == 'text/csv'
+arq = st.file_uploader("Carregue os arquivos", type={"csv", "geojson"},accept_multiple_files=True)
+if arq is not None:
+    csv_arq = None
+    geo_arq = None
+    for arquivo in arq:
+        if arquivo.type == 'application/vnd.geo+json':
+            geo_arq = arquivo
+        elif arquivo.type == 'text/csv':
             csv_arq = arquivo
-    if csv_arq is not None:
-        data = pd.read_csv(csv_arq)
-    else:
-        st.warning("Por favor, carregue o arquivo CSV.")
+    # Verifica se ambos os arquivos foram carregados
+if csv_arq is not None and geo_arq is not None:
+    arq_gpd = gpd.read_file(geo_arq)
+    arq_pd = pd.read_csv(csv_arq)
+    data = arq_gpd.merge(arq_pd, on="Município")
 else:
-    arq = st.file_uploader("Carregue o arquivo", type={"gdf"})
-    if arq is not None:
-        gdf_arq = None
-        for arquivo in arq:
-            arquivo.type == 'application/vnd.gdf'
-            gdf_arq = arquivo
-    if gdf_arq is not None:
-        data = gdf_arq
-    else:
-        st.warning("Por favor, carregue um arquivo GeoDataFrame (gdf)")
-
-
-
-
+    st.warning("Por favor, carregue um arquivo CSV e um arquivo GeoJSON.")
