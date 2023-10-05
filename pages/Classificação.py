8b12950291c98518442490aa196449272e1b09ae
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-10-04 21:46:56 -0300

    teste_class

--- pages/Classificação.py
+++ pages/Classificação.py
@@ -103,7 +103,7 @@ ponto_central = arq_geojson.geometry.centroid
 lat = ponto_central.iloc[0].y
 lon = ponto_central.iloc[0].x
     
-if not isinstance(data,gpd.GeoDataFrame):
+if not isinstance(gdf,gpd.GeoDataFrame):
     print("O arquivo não é um GeoDataFrame")
     exit()
 
