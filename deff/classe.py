6623ac5793750357885aa6ff9a09b303bbbd7e46
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-10-06 19:48:20 -0300

    teste_fonte

--- deff/classe.py
+++ deff/classe.py
@@ -66,9 +66,9 @@ def gvf (area,arq,comum,ind,scheme,k,cmap):
         GVF = (100 - ((SDCM / SDAM) * 100)).round(2)
 
         if GVF < 80:
-            st.markdown(f"<p> <font style='font-weight: bold; color='red'><font size='+5'> GVF = {GVF} </font></p>",unsafe_allow_html=True)
+            st.markdown(f"<p> <font weight: bold><font color='red'><font size='+5'> GVF = {GVF} </font></p>",unsafe_allow_html=True)
         else:
-            st.markdown(f"<p><font style='font-weight: bold'><font size='+5'> GVF = {GVF} </font></p>",unsafe_allow_html=True)
+            st.markdown(f"<p><font weight: bold><font size='+5'> GVF = {GVF} </font></p>",unsafe_allow_html=True)
 
 #f"<p style='line-height: 0.7;'><font size='+10' color='#58326b'>{arrow_u}</font> <font size='+5'>{max_str} = {ind_mx} {unidade}</font></p>
 #st.markdown(f"<p> <font style='font-weight: bold; color='red'><font size='+5'> GVF = {GVF:.2f} </font> %</font></p>",unsafe_allow_html=True)
