d3e6406667aca865f7e6dcf72e793f3c2320624f
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-10-05 22:24:48 -0300

    teste

--- pages/Classificação.py
+++ pages/Classificação.py
@@ -44,9 +44,9 @@ with c1:
     cmap = st.text_input('Paleta de cores:', placeholder = "Digite o nome da paleta de cores.")
 
     st.markdown("Deseja comparar diferentes classificações produzindo dois mapas?")
-    on = form.radio('Comparar duas classificações', ['Sim','Não'])
+    on = form.toggle('Comparar duas classificações')
 
-    if on == 'Sim':
+    if on:
         scheme1 = st.text_input('Método de classificação 2:', placeholder = "Digite o método de classificação.")
         k1 = int(st.number_input("Número de classes 2", placeholder="Digite o número de classes que os dados serão divididos."))
         cmap1 = st.text_input('Paleta de cores 2:', placeholder = "Digite o nome da paleta de cores.")
