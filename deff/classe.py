65c677e2e01221cf80b4b153c85b0c8858cfbcce
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-10-09 09:42:17 -0300

    Update classe.py

--- deff/classe.py
+++ deff/classe.py
@@ -58,7 +58,7 @@ def gvf (area,arq,comum,ind,scheme,k,cmap):
             Z.append(np.sum(sq))  # calcula e guarda a soma do quadrado da diferença por essa classe
 
 #####
-            num_colunas = st.number_input("Número de colunas para exibição:", min_value=1, max_value=len(q.bins), value=1)
+            num_colunas = k
 
             result = []
             for i, (intervalo, contagem) in enumerate(zip(q.bins, q.counts)):
