05c6726810e399d4245932cb0cf6b85ad2ba75e1
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-10-12 17:46:05 -0300

    teste

--- pages/teste.py
+++ pages/teste.py
@@ -23,41 +23,10 @@ NTC =  "./dados/geojson/NTC.geojson"
 
 st.set_page_config(layout="wide")
 
-# Remove whitespace from the top of the page and sidebar
-st.markdown("""
-        <style>
-               .block-container {
-                    padding-top: 1rem;
-                    padding-bottom: 0rem;
-                    padding-left: 5rem;
-                    padding-right: 5rem;
-                }
-        </style>
-        """, unsafe_allow_html=True)
 
-colored_header(label="Renda média da população",
-               description="Renda média da população no Paraná",
-               color_name="red-70",)
-
-
-
-#Arquivos
-PR = 'https://raw.githubusercontent.com/jhbricks/mapades/main/dados/geojson/PR.geojson'
 NTC = 'https://raw.githubusercontent.com/jhbricks/mapades/main/dados/geojson/NTC.geojson'
-renda = "./dados/csv/renda.csv"
-
-#Selecionar a área [Radio horizontal]
-#st.markdown("<h3><font size='8'  color='red'>Renda</font></font></h3>", unsafe_allow_html=True)
-area = st.radio("Selecione uma área:",("Paraná","Núcleo Territorial Central"))
-st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
-
-if area == "Paraná":
-     t1, t2, t3, t4 = st.tabs(["Coeficiente de Gini", "Renda média da população", "Renda da população feminina", "Renda dos declarantes do IRPF"])
-     with t1:
-          st.markdown('AHAHAJNJAZ')
-          NTC = 'https://raw.githubusercontent.com/jhbricks/mapades/main/dados/geojson/NTC.geojson'
-          m = leafmap.Map(center=[-51.8, -24.7])
-          style = {"color": "#000000","fillOpacity": 0,"clickable": False}
-          m.add_geojson(NTC, style = {"color": "#000000","fillOpacity": 0,"clickable": False})
-          m.to_streamlit()
+m = leafmap.Map(center=[-51.8, -24.7])
+style = {"color": "#000000","fillOpacity": 0,"clickable": False}
+m.add_geojson(NTC, style = {"color": "#000000","fillOpacity": 0,"clickable": False})
+m.to_streamlit()
      
