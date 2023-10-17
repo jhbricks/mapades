37559540ded6cfc825604e63400a6e545f8c6c31
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-10-17 18:01:43 -0300

    teste

--- pages/teste.py
+++ pages/teste.py
@@ -65,7 +65,7 @@ if area == "Paraná":
 #######LAT E LON CENTRAIS
       lon, lat = leafmap.gdf_centroid(data)
 ##########################MAPA
-      style = {'lineColor': '#000000'} 
+      style = {'Color': '#000000'} 
 ########MAPA INICIAL
       m = leafmap.Map(center=(lat,lon),draw_control=False,measure_control=False,fullscreen_control=False,attribution_control=True)
 #######ADICIONAR O MERGE GDF
