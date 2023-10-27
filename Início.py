47d065e0c5d45ad5241520850301bb66c769a7ea
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-10-26 23:19:11 -0300

    layout

--- Início.py
+++ Início.py
@@ -5,16 +5,7 @@ from streamlit_extras.switch_page_button import switch_page
 st.set_page_config(layout="wide",page_title="Mapa da Desigualdade")
 
 #Remove os espaços em branco no topo
-st.markdown("""
-        <style>
-               .block-container {
-                    padding-top: 1rem;
-                    padding-bottom: 0rem;
-                    padding-left: 5rem;
-                    padding-right: 5rem;
-                }
-        </style>
-        """, unsafe_allow_html=True)
+st.markdown("""<style> .block-container {padding-top: 1rem;}</style> """, unsafe_allow_html=True)
 
 st.write("# Mapa da Desigualdade")
 
@@ -26,12 +17,12 @@ st.markdown("**Selecione um dos temas abaixo ou abra o menu ao lado** ")
 m = st.markdown("""
 <style>
 div.stButton > button:first-child {
-    background-color: #faecca;
+    background-color: #fad3ac;
     color: black;
     height: 3em;
     width: 12em;
     border-radius:10px;
-    border:3px solid #faecca;
+    border:3px solid #fad3ac;
     font-size:20px;
     font-weight: bold;
     margin: auto;
@@ -39,8 +30,8 @@ div.stButton > button:first-child {
 }
 
 div.stButton > button:hover {
-	background:linear-gradient(to bottom, #faecca 5%, #f0ede6 100%);
-	background-color:##faecca;
+	background:linear-gradient(to bottom, #fad3ac 5%, #ffe8d1 100%);
+	background-color:#fad3ac;
 }
 </style>""", unsafe_allow_html=True)        
 
@@ -52,35 +43,35 @@ with c1:
     if cont:
         switch_page("Contextualização")
 
-    seg = st.button("Segurança")
-    if seg:
-        switch_page("Segurança")
-
-    out = st.button("Mais temas")
-    if out:
-        switch_page("Mais temas")
-
-with c2:
     renda = st.button("Renda")
     if renda:
         switch_page("Renda")
+
+    riqz = st.button("Riqueza")
+    if riqz:
+        switch_page("Riqueza")
+
+with c2:
+    seg = st.button("Segurança")
+    if seg:
+        switch_page("Segurança")
 
     amb = st.button("Meio ambiente")
     if amb:
         switch_page("Meio ambiente")
 
-    sob = st.button("Sobre")
-    if sob:
-        switch_page("Sobre")
-
-with c3:
-    riqz = st.button("Riqueza")
-    if riqz:
-        switch_page("Riqueza")
-
     edu = st.button("Educação")
     if edu:
         switch_page("Educação")
+
+with c3:
+    mais = st.button("Mais temas")
+    if mais:
+        switch_page("Mais temas")
+
+    sob = st.button("Sobre")
+    if sob:
+        switch_page("Sobre")
 
     aju = st.button("Ajuda")
     if aju:
