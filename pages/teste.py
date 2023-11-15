24766693ae9f41643d01e8b6d0f7d9bf22400fb9
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-11-15 15:28:04 -0300

    teste

--- pages/teste.py
+++ pages/teste.py
@@ -15,4 +15,22 @@ contexto = "./dados/csv/contexto.csv"
 pop = "./dados/csv/pop_2021.csv"
 
 if op == "A":
-      mapa('PR',contexto,'População','FisherJenks',5,'Wistia', ['Município','População'],'População residente (hab)')
+      a,b,c,d,e,f = st.columns()
+      with a:
+            mapa('PR',contexto,'População','FisherJenks',5,'Wistia', ['Município','População'],'População residente (hab)')
+            st.text("Wistia")
+      with b:
+            mapa('PR',contexto,'População','FisherJenks',5,'YlGnBu', ['Município','População'],'População residente (hab)')
+            st.text("YlGnBu")
+      with c:
+            mapa('PR',contexto,'População','FisherJenks',5,'YlGn', ['Município','População'],'População residente (hab)')
+            st.text("YlGn")
+      with d:
+            mapa('PR',contexto,'População','FisherJenks',5,'YlOrRd', ['Município','População'],'População residente (hab)')
+            st.text("YlOrRd")
+      with e:
+            mapa('PR',contexto,'População','FisherJenks',5,'autumn_r', ['Município','População'],'População residente (hab)')
+            st.text("autumn_r")
+      with f:
+            mapa('PR',contexto,'População','FisherJenks',5,'cividis_r', ['Município','População'],'População residente (hab)')
+            st.text("cividis_r")
