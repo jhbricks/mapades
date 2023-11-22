b51ba39d9f6de8b3b7aed0a08e12a1306a7d2f5d
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-11-22 17:54:32 -0300

    UP

--- pages/Comparação.py
+++ pages/Comparação.py
@@ -10,13 +10,13 @@ a1,a2 = st.columns(2)
 with a1:
     cat1 = st.selectbox("Escolha uma categoria:",("Contextualização","Renda","Riqueza"),index=None,placeholder="Selecione uma categoria...")
     if cat1 == "Contextualização":
-        ind1 = st.selectbox("Escolha um indicador de contexto:",("População","Densidade demográfica","Grau de urbanização","População feminina","População preta/parda","Razão de dependência"),
+        ind1 = st.selectbox("Escolha um indicador de Contexto:",("População","Densidade demográfica","Grau de urbanização","População feminina","População preta/parda","Razão de dependência"),
                           index=None,placeholder="Selecione um indicador...")
     elif cat1 == "Renda":
-        ind1 = st.selectbox("Escolha um indicador de renda:",("Índice Gini","Renda média da população","Renda da população feminina","Renda dos declarantes do IRPF"),
+        ind1 = st.selectbox("Escolha um indicador de Renda:",("Índice Gini","Renda média da população","Renda da população feminina","Renda dos declarantes do IRPF"),
                            index=None,placeholder="Selecione um indicador...")
     elif cat1 == "Riqueza":
-        ind1 = st.selectbox("Escolha um indicador de riqueza:",("Domicílios com bens duráveis","Número de veículos por pessoas","População declarante do IRPF","Patrimônio líquido médio da população","Patrimônio líquido médio dos declarantes do IRPF"),
+        ind1 = st.selectbox("Escolha um indicador de Riqueza:",("Domicílios com bens duráveis","Número de veículos por pessoas","População declarante do IRPF","Patrimônio líquido médio da população","Patrimônio líquido médio dos declarantes do IRPF"),
                            index=None,placeholder="Selecione um indicador...")
 with a2:
     cat2 = st.selectbox("Escolha uma categoria:",("Contextualização","Renda","Riqueza"),index=None,placeholder="Selecione uma categoria...")
