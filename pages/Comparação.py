7d1b286b6d24c5a245bfbf92cd34ca9d4a466046
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-11-22 18:07:37 -0300

    up

--- pages/Comparação.py
+++ pages/Comparação.py
@@ -8,10 +8,10 @@ st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', uns
 a1,a2 = st.columns(2)
 
 with a1:
-    cat1 = st.selectbox("Escolha uma categoria:",("Contextualização","Renda","Riqueza"),index=None,placeholder="Selecione uma categoria...")
+    cat1 = st.selectbox("Escolha uma categoria:",("Contextualização","Renda","Riqueza"),index=None,placeholder="Selecione uma categoria...",max_selections=2)
     if cat1 == "Contextualização":
         ind1 = st.selectbox("Escolha um indicador de Contexto:",("População","Densidade demográfica","Grau de urbanização","População feminina","População preta/parda","Razão de dependência"),
-                          index=None,placeholder="Selecione um indicador...")
+                          index=None,placeholder="Selecione um indicador...",max_selections = 2)
     elif cat1 == "Renda":
         ind1 = st.selectbox("Escolha um indicador de Renda:",("Índice Gini","Renda média da população","Renda da população feminina","Renda dos declarantes do IRPF"),
                            index=None,placeholder="Selecione um indicador...")
