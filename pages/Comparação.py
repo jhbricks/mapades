603449364a54075e6eebf314d1676cbfc2f0fd93
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-11-24 11:01:45 -0300

    a

--- pages/Comparação.py
+++ pages/Comparação.py
@@ -9,12 +9,13 @@ st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', uns
 renda = "./dados/csv/renda.csv"
 contexto = "./dados/csv/contexto.csv"
 
-form = st.form(key="form_settings")
 
 
-a1,a2 = form.columns(2)
+a1,a2 = st.columns(2)
 
 with a1:
+    form = st.form(key="mapa1")
+
     cat1 = st.selectbox("Escolha uma categoria:",("Contextualização","Renda","Riqueza"),placeholder="Selecione uma categoria...")
     #form.form_submit_button(label="Submit")
 
@@ -30,4 +31,25 @@ with a1:
         ind1 = form.selectbox("Escolha um indicador de Riqueza:",("Domicílios com bens duráveis","Número de veículos por pessoas","População declarante do IRPF","Patrimônio líquido médio da população","Patrimônio líquido médio dos declarantes do IRPF"),
                               placeholder="Selecione um indicador...")
 
-form.form_submit_button(label="Submit")
+    form.form_submit_button(label="Submit")
+
+
+with a2:
+    form = st.form(key="mapa2")
+
+    cat1 = st.selectbox("Escolha uma categoria:",("Contextualização","Renda","Riqueza"),placeholder="Selecione uma categoria...")
+    #form.form_submit_button(label="Submit")
+
+#form = st.form(key="form_settings1")
+
+    if cat1 == "Contextualização":
+        ind1 = form.selectbox("Escolha um indicador de Contexto:",("População","Densidade demográfica","Grau de urbanização","População feminina","População preta/parda","Razão de dependência"),
+                              placeholder="Selecione um indicador...")
+    elif cat1 == "Renda":
+        ind1 = form.selectbox("Escolha um indicador de Renda:",("Índice Gini","Renda média da população","Renda da população feminina","Renda dos declarantes do IRPF"),
+                              placeholder="Selecione um indicador...")
+    elif cat1 == "Riqueza":
+        ind1 = form.selectbox("Escolha um indicador de Riqueza:",("Domicílios com bens duráveis","Número de veículos por pessoas","População declarante do IRPF","Patrimônio líquido médio da população","Patrimônio líquido médio dos declarantes do IRPF"),
+                              placeholder="Selecione um indicador...")
+
+    form.form_submit_button(label="Submit")
