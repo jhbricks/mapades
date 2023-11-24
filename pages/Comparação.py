43421196f53e41bf8bb339511bc17afe7b324c86
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-11-24 11:16:55 -0300

    a

--- pages/Comparação.py
+++ pages/Comparação.py
@@ -16,18 +16,18 @@ a1,a2 = st.columns(2)
 with a1:
     form = st.form(key="mapa1")
 
-    cat1 = form.selectbox("Escolha uma categoria:",("Contextualização","Renda","Riqueza"),key='mapa1',placeholder="Selecione uma categoria...")
+    cat1 = form.selectbox("Escolha uma categoria:",("Contextualização","Renda","Riqueza"),key='mapa1',index=None,placeholder="Selecione uma categoria...")
 
 
     if cat1 == "Contextualização":
         ind1 = form.selectbox("Escolha um indicador de Contexto:",("População","Densidade demográfica","Grau de urbanização","População feminina","População preta/parda","Razão de dependência"),
-                              placeholder="Selecione um indicador...")
+                              index=None,placeholder="Selecione um indicador...")
     elif cat1 == "Renda":
         ind1 = form.selectbox("Escolha um indicador de Renda:",("Índice Gini","Renda média da população","Renda da população feminina","Renda dos declarantes do IRPF"),
-                              placeholder="Selecione um indicador...")
+                              index=None,placeholder="Selecione um indicador...")
     elif cat1 == "Riqueza":
         ind1 = form.selectbox("Escolha um indicador de Riqueza:",("Domicílios com bens duráveis","Número de veículos por pessoas","População declarante do IRPF","Patrimônio líquido médio da população","Patrimônio líquido médio dos declarantes do IRPF"),
-                              placeholder="Selecione um indicador...")
+                              index=None,placeholder="Selecione um indicador...")
 
     form.form_submit_button(label="Submit")
 
@@ -39,23 +39,27 @@ with a1:
 with a2:
     form = st.form(key="mapa2")
 
-    cat1 = form.selectbox("Escolha uma categoria:",("Contextualização","Renda","Riqueza"),key='mapa2',placeholder="Selecione uma categoria...")
+    cat1 = form.selectbox("Escolha uma categoria:",("Contextualização","Renda","Riqueza"),key='mapa2',index=None,placeholder="Selecione uma categoria...")
     #form.form_submit_button(label="Submit")
 
 #form = st.form(key="form_settings1")
 
     if cat1 == "Contextualização":
         ind1 = form.selectbox("Escolha um indicador de Contexto:",("População","Densidade demográfica","Grau de urbanização","População feminina","População preta/parda","Razão de dependência"),
-                              placeholder="Selecione um indicador...")
+                              index=None,placeholder="Selecione um indicador...")
     elif cat1 == "Renda":
         ind1 = form.selectbox("Escolha um indicador de Renda:",("Índice de Gini","Renda média da população","Renda da população feminina","Renda dos declarantes do IRPF"),
-                              placeholder="Selecione um indicador...")
+                              index=None,placeholder="Selecione um indicador...")
     elif cat1 == "Riqueza":
         ind1 = form.selectbox("Escolha um indicador de Riqueza:",("Domicílios com bens duráveis","Número de veículos por pessoas","População declarante do IRPF","Patrimônio líquido médio da população","Patrimônio líquido médio dos declarantes do IRPF"),
-                              placeholder="Selecione um indicador...")
+                              index=None,placeholder="Selecione um indicador...")
 
     form.form_submit_button(label="Submit")
     if ind1 == 'Índice de Gini':
         mapa('PR',renda,'Índice de Gini','FisherJenks',3,'PuBuGn', ['Município','Índice de Gini'],'Índice de Gini')
+
+
+form = st.form(key="mapa3")
+
 
 
