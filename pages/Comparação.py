d85a8fca9d70b75b0b1205b4a8e64c665005dab0
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-11-27 18:43:14 -0300

    dict

--- pages/Comparação.py
+++ pages/Comparação.py
@@ -9,62 +9,60 @@ st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', uns
 renda = "./dados/csv/renda.csv"
 contexto = "./dados/csv/contexto.csv"
 
+categoria_p = {"Contextualização" : {
+"População residente":mapa('PR',contexto,'População','FisherJenks',5,'copper_r', ['Município','População'],'População (hab)'),
+"Densidade demográfica":mapa('PR',contexto,'Densidade Demográfica (hab/km²)','FisherJenks',5,'YlOrRd', ['Município','Densidade Demográfica (hab/km²)'],'Densidade Demográfica (hab/km²)'),
+"Grau de urbanização":mapa('PR',contexto,'Grau de Urbanização (%)','FisherJenks',4,'summer_r', ['Município','Grau de Urbanização (%)'],'Grau de Urbanização (%)'),
+"População feminina": mapa('PR',contexto, 'População feminina (%)', 'FisherJenks',3,'Reds', ['Município','População feminina (%)'],'População feminina (%)'),
+"População preta/parda":mapa('PR',contexto, 'População preta ou parda (%)', 'FisherJenks', 5, 'YlGnBu', ['Município','População preta ou parda (%)'],'População preta ou parda (%)'),
+"Razão de dependência":mapa('PR',contexto, 'Razão de Dependência (%)', 'FisherJenks', 3, 'Purples', ['Município','Razão de Dependência (%)'],'Razão de Dependência (%)')
+},
+"Renda" : {
+"Índice de Gini":mapa('PR',renda,'Índice de Gini','FisherJenks',3,'PuBuGn', ['Município','Índice de Gini'],'Índice de Gini'),
+"Renda média da população":mapa('PR',renda,'Renda Média da População (R$ mil)','FisherJenks',5,'YlOrRd', ['Município','Renda Média da População (R$ mil)'],'Renda Média da População (R$ mil)'),
+"Rendimento médio da população feminina":mapa('PR',renda,'Rendimento médio da população feminina/masculina (%)','FisherJenks',5,'RdPu', ['Município','Rendimento médio da população feminina/masculina (%)'],'Rendimento médio da população feminina (%)'),
+"Renda dos declarantes do IRPF":mapa('PR',renda,'Renda Média dos Declarantes (R$ mil)','FisherJenks',5,'YlGn', ['Município','Renda Média dos Declarantes (R$ mil)'],'Renda Média dos Declarantes (R$ mil)'),
+},
+"Riqueza" : {
+"Domicílios com bens duráveis":mapa('PR', riqueza, 'Domicílios com bens duráveis (%)','FisherJenks', 5, 'OrRd', ['Município','Domicílios com bens duráveis (%)'],'Domicílios com bens duráveis (%)'),
+"Número de veículos por pessoas":mapa('PR', riqueza, 'Veículos por pessoa','FisherJenks', 4, 'BuGn', ['Município','Veículos por pessoa'],'Número de veículos por pessoas'),
+"População declarante do IRPF":mapa('PR', riqueza, 'Declarantes do IRPF (%)','FisherJenks',5, 'BuPu', ['Município','Declarantes do IRPF (%)'],'População declarante do IRPF (%)'),
+"Patrimônio Líquido Médio da População":mapa('PR', riqueza,'Patrimônio líquido médio da população (R$ milhões)','FisherJenks',4,'YlOrBr',['Município','Patrimônio líquido médio da população (R$ milhões)'],'Patrimônio líquido médio da população (R$ milhões)'),
+"Patrimônio Líquido Médio dos declarantes do IRPF":mapa('PR', riqueza, 'Patrimônio líquido médio dos declarantes (R$ milhões)','FisherJenks', 5, 'GnBu', ['Município','Patrimônio líquido médio dos declarantes (R$ milhões)'],'Patrimônio líquido dos declarantes do IRPF (R$ milhões)'),
+}}
 
 
 a1,a2 = st.columns(2)
 
 with a1:
-    form = st.form(key="mapa1")
+    form1 = st.form(key="mapa1")
+    cat1 = form1.selectbox("Escolha uma categoria:", list(categoria_p.keys()), key='mapa1',
+                           index=None, placeholder="Selecione uma categoria...")
 
-    cat1 = form.selectbox("Escolha uma categoria:",("Contextualização","Renda","Riqueza"),key='mapa1',index=None,placeholder="Selecione uma categoria...")
+    ind1 = form1.selectbox("Escolha um indicador:", list(categoria_p[cat1].keys()),
+                           index=None, placeholder="Selecione um indicador...")
 
-
-    if cat1 == "Contextualização":
-        ind1 = form.selectbox("Escolha um indicador de Contexto:",("População","Densidade demográfica","Grau de urbanização","População feminina","População preta/parda","Razão de dependência"),
-                              index=None,placeholder="Selecione um indicador...")
-    elif cat1 == "Renda":
-        ind1 = form.selectbox("Escolha um indicador de Renda:",("Índice Gini","Renda média da população","Renda da população feminina","Renda dos declarantes do IRPF"),
-                              index=None,placeholder="Selecione um indicador...")
-    elif cat1 == "Riqueza":
-        ind1 = form.selectbox("Escolha um indicador de Riqueza:",("Domicílios com bens duráveis","Número de veículos por pessoas","População declarante do IRPF","Patrimônio líquido médio da população","Patrimônio líquido médio dos declarantes do IRPF"),
-                              index=None,placeholder="Selecione um indicador...")
-
-    form.form_submit_button(label="Submit")
-
-
-
-
+    form1.form_submit_button(label="Submit")
 
 with a2:
-    form = st.form(key="mapa2")
+    form2 = st.form(key="mapa2")
+    cat2 = form2.selectbox("Escolha uma categoria:", list(categoria_p.keys()), key='mapa2',
+                           index=None, placeholder="Selecione uma categoria...")
 
-    cat1 = form.selectbox("Escolha uma categoria:",("Contextualização","Renda","Riqueza"),key='mapa2',index=None,placeholder="Selecione uma categoria...")
-    #form.form_submit_button(label="Submit")
+    ind2 = form2.selectbox("Escolha um indicador:", list(categoria_p[cat2].keys()),
+                           index=None, placeholder="Selecione um indicador...")
 
-#form = st.form(key="form_settings1")
+    form2.form_submit_button(label="Submit")
 
-    if cat1 == "Contextualização":
-        ind1 = form.selectbox("Escolha um indicador de Contexto:",("População","Densidade demográfica","Grau de urbanização","População feminina","População preta/parda","Razão de dependência"),
-                              index=None,placeholder="Selecione um indicador...")
-    elif cat1 == "Renda":
-        ind1 = form.selectbox("Escolha um indicador de Renda:",("Índice de Gini","Renda média da população","Renda da população feminina","Renda dos declarantes do IRPF"),
-                              index=None,placeholder="Selecione um indicador...")
-    elif cat1 == "Riqueza":
-        ind1 = form.selectbox("Escolha um indicador de Riqueza:",("Domicílios com bens duráveis","Número de veículos por pessoas","População declarante do IRPF","Patrimônio líquido médio da população","Patrimônio líquido médio dos declarantes do IRPF"),
-                              index=None,placeholder="Selecione um indicador...")
+# Assuming your mapa function takes the selected category and indicator as arguments
+if st.form_submit_button("Generate Map"):
+    if cat1 in categoria_p and ind1 in categoria_p[cat1]:
+        params1 = categoria_p[cat1][ind1]
+        mapa(*params1)  # Adjust this line according to the signature of the mapa function
 
-    form.form_submit_button(label="Submit")
-
-
-with a1:
-    if ind1 == 'População':
-        mapa('PR',contexto,'População','FisherJenks',5,'copper_r', ['Município','População'],'População (hab)')
-with a2:
-    if ind1 == 'Índice de Gini':
-        mapa('PR',renda,'Índice de Gini','FisherJenks',3,'PuBuGn', ['Município','Índice de Gini'],'Índice de Gini')
-
-
-
+    if cat2 in categoria_p and ind2 in categoria_p[cat2]:
+        params2 = categoria_p[cat2][ind2]
+        mapa(*params2)
 
 
 
--- pages/teste_classe.py
+++ pages/teste_classe.py
@@ -1,81 +1,111 @@
-import dash
-from dash import dcc, html
-from dash.dependencies import Input, Output
-import plotly.express as px
-import geopandas as gpd
-from deff.mapa import mapa
-from deff.mapa import grafico
-from deff.calculos import conta
+#Paraná
+"Contextualização" = {
+"População residente":mapa('PR',contexto,'População','FisherJenks',5,'copper_r', ['Município','População'],'População (hab)'),
+"Densidade demográfica":mapa('PR',contexto,'Densidade Demográfica (hab/km²)','FisherJenks',5,'YlOrRd', ['Município','Densidade Demográfica (hab/km²)'],'Densidade Demográfica (hab/km²)'),
+"Grau de urbanização":mapa('PR',contexto,'Grau de Urbanização (%)','FisherJenks',4,'summer_r', ['Município','Grau de Urbanização (%)'],'Grau de Urbanização (%)'),
+"População feminina": mapa('PR',contexto, 'População feminina (%)', 'FisherJenks',3,'Reds', ['Município','População feminina (%)'],'População feminina (%)'),
+"População preta/parda":mapa('PR',contexto, 'População preta ou parda (%)', 'FisherJenks', 5, 'YlGnBu', ['Município','População preta ou parda (%)'],'População preta ou parda (%)'),
+"Razão de dependência":mapa('PR',contexto, 'Razão de Dependência (%)', 'FisherJenks', 3, 'Purples', ['Município','Razão de Dependência (%)'],'Razão de Dependência (%)')
+}
 
-# Create Dash app
-app = dash.Dash(__name__)
+"Renda" = {
+"Índice de Gini":mapa('PR',renda,'Índice de Gini','FisherJenks',3,'PuBuGn', ['Município','Índice de Gini'],'Índice de Gini'),
+"Renda média da população":mapa('PR',renda,'Renda Média da População (R$ mil)','FisherJenks',5,'YlOrRd', ['Município','Renda Média da População (R$ mil)'],'Renda Média da População (R$ mil)'),
+"Rendimento médio da população feminina":mapa('PR',renda,'Rendimento médio da população feminina/masculina (%)','FisherJenks',5,'RdPu', ['Município','Rendimento médio da população feminina/masculina (%)'],'Rendimento médio da população feminina (%)'),
+"Renda dos declarantes do IRPF":mapa('PR',renda,'Renda Média dos Declarantes (R$ mil)','FisherJenks',5,'YlGn', ['Município','Renda Média dos Declarantes (R$ mil)'],'Renda Média dos Declarantes (R$ mil)'),
+}
 
-# App layout
-app.layout = html.Div([
-    html.H3("Contextualização - Mapa da Desigualdade"),
-    
-    # Select area
-    html.H3("Contextualização"),
-    dcc.RadioItems(
-        id='area-selector',
-        options=[
-            {'label': 'Paraná', 'value': 'PR'},
-            {'label': 'Núcleo Territorial Central de Curitiba', 'value': 'NTCC'},
-        ],
-        value='PR',
-        labelStyle={'display': 'block'}
-    ),
+"Riqueza" = {
+"Domicílios com bens duráveis":mapa('PR', riqueza, 'Domicílios com bens duráveis (%)','FisherJenks', 5, 'OrRd', ['Município','Domicílios com bens duráveis (%)'],'Domicílios com bens duráveis (%)'),
+"Número de veículos por pessoas":mapa('PR', riqueza, 'Veículos por pessoa','FisherJenks', 4, 'BuGn', ['Município','Veículos por pessoa'],'Número de veículos por pessoas'),
+"População declarante do IRPF":mapa('PR', riqueza, 'Declarantes do IRPF (%)','FisherJenks',5, 'BuPu', ['Município','Declarantes do IRPF (%)'],'População declarante do IRPF (%)'),
+"Patrimônio Líquido Médio da População":mapa('PR', riqueza,'Patrimônio líquido médio da população (R$ milhões)','FisherJenks',4,'YlOrBr',['Município','Patrimônio líquido médio da população (R$ milhões)'],'Patrimônio líquido médio da população (R$ milhões)'),
+"Patrimônio Líquido Médio dos declarantes do IRPF":mapa('PR', riqueza, 'Patrimônio líquido médio dos declarantes (R$ milhões)','FisherJenks', 5, 'GnBu', ['Município','Patrimônio líquido médio dos declarantes (R$ milhões)'],'Patrimônio líquido dos declarantes do IRPF (R$ milhões)'),
+}
+#NTC
+"População residente":mapa('NTC',contexto,'População','FisherJenks',4,'copper_r', ['Município','População'],'População (hab)')
+"Densidade demográfica":mapa('NTC',contexto,'Densidade Demográfica (hab/km²)','FisherJenks',4,'YlOrRd', ['Município','Densidade Demográfica (hab/km²)'],'Densidade Demográfica (hab/km²)')
+"Grau de urbanização":mapa('NTC',contexto,'Grau de Urbanização (%)','FisherJenks',3,'summer_r', ['Município','Grau de Urbanização (%)'],'Grau de Urbanização (%)')
+"População feminina": mapa('NTC',contexto, 'População feminina (%)', 'FisherJenks',3,'Reds', ['Município','População feminina (%)'],'População feminina (%)')
+"População preta/parda":mapa('NTC',contexto, 'População preta ou parda (%)', 'FisherJenks', 4, 'YlGnBu', ['Município','População preta ou parda (%)'],'População preta ou parda (%)')
+"Razão de dependência":mapa('NTC',contexto, 'Razão de Dependência (%)', 'FisherJenks', 4, 'Purples', ['Município','Razão de Dependência (%)'],'Razão de Dependência (%)')
 
-    # Select indicator
-    dcc.RadioItems(
-        id='indicator-selector',
-        options=[
-            {'label': 'Localização', 'value': 'Location'},
-            {'label': 'População residente', 'value': 'Population'},
-            {'label': 'Densidade demográfica', 'value': 'Density'},
-            {'label': 'Grau de urbanização', 'value': 'Urbanization'},
-            {'label': 'População feminina', 'value': 'FemalePopulation'},
-            {'label': 'População preta/parda', 'value': 'BlackBrownPopulation'},
-            {'label': 'Razão de dependência', 'value': 'DependencyRatio'},
-        ],
-        value='Population',
-        labelStyle={'display': 'block'}
-    ),
+"Índice de Gini":mapa('NTC',renda,'Índice de Gini','FisherJenks',4,'PuBuGn', ['Município','Índice de Gini'],'Índice de Gini')
+"Renda média da população":mapa('NTC',renda,'Renda Média da População (R$ mil)','FisherJenks',4,'YlOrRd', ['Município','Renda Média da População (R$ mil)'],'Renda Média da População (R$ mil)')
+"Rendimento médio da população feminina":mapa('NTC',renda,'Rendimento médio da população feminina/masculina (%)','FisherJenks',4,'RdPu', ['Município','Rendimento médio da população feminina/masculina (%)'],'Rendimento médio da população feminina (%)')
+"Renda dos declarantes do IRPF":mapa('NTC',renda,'Renda Média dos Declarantes (R$ mil)','FisherJenks',4,'YlGn', ['Município','Renda Média dos Declarantes (R$ mil)'],'Renda Média dos Declarantes (R$ mil)')
 
-    # Display Map and Graph
-    html.Div(id='map-container'),
-    html.Div(id='graph-container'),
+"Domicílios com bens duráveis":mapa('NTC', riqueza, 'Domicílios com bens duráveis (%)','FisherJenks', 4, 'OrRd', ['Município','Domicílios com bens duráveis (%)'],'Domicílios com bens duráveis (%)')
+"Número de veículos por pessoas":mapa('NTC', riqueza, 'Veículos por pessoa','FisherJenks', 4, 'BuGn', ['Município','Veículos por pessoa'],'Número de veículos por pessoas')
+"População declarante do IRPF":mapa('NTC', riqueza, 'Declarantes do IRPF (%)','FisherJenks',4, 'BuPu', ['Município','Declarantes do IRPF (%)'],'População declarante do IRPF (%)')
+"Patrimônio Líquido Médio da População":mapa('NTC', riqueza, 'Patrimônio líquido médio da população (R$ milhões)','FisherJenks', 4, 'YlOrBr', ['Município','Patrimônio líquido médio da população (R$ milhões)'],'Patrimônio líquido médio da população (R$ milhões)')
+"Patrimônio Líquido Médio dos declarantes do IRPF":mapa('NTC', riqueza, 'Patrimônio líquido médio dos declarantes (R$ milhões)','FisherJenks', 4, 'GnBu', ['Município','Patrimônio líquido médio dos declarantes (R$ milhões)'],'Patrimônio líquido médio dos declarantes do IRPF (R$ milhões)')
 
-    # Additional Information
-    html.Div([
-        html.P("Ano-base: 2021"),
-        html.P("Fonte(s): IBGE"),
-        html.P("Fórmula: População total por município"),
-        html.P("Observações: Prévia da população por município do Censo Demográfico 2022 do IBGE.")
-    ]),
-])
 
-# Callbacks to update map and graph based on user selections
-@app.callback(
-    [Output('map-container', 'children'),
-     Output('graph-container', 'children')],
-    [Input('area-selector', 'value'),
-     Input('indicator-selector', 'value')]
-)
-def update_content(selected_area, selected_indicator):
-    map_figure = None
-    graph_figure = None
 
-    if selected_area == 'PR':
-        if selected_indicator == 'Population':
-            map_figure = mapa('PR', contexto, 'População', 'FisherJenks', 5, 'copper_r', ['Município', 'População'], 'População (hab)')
-            graph_figure = conta('PR', contexto, 'População', 2021, 'População total', 'soma', 'habitantes')
-            graph_figure = grafico('PR', contexto, 'População', 'Habitantes')
 
-        # Add more conditions for other indicators...
 
-    return map_figure, graph_figure
 
-# Run the app
-if __name__ == '__main__':
-    app.run_server(debug=True)
+
+
+
+
+a1,a2 = st.columns(2)
+
+with a1:
+    form = st.form(key="mapa1")
+
+    cat1 = form.selectbox("Escolha uma categoria:",("Contextualização","Renda","Riqueza"),key='mapa1',index=None,placeholder="Selecione uma categoria...")
+
+
+    if cat1 == "Contextualização":
+        ind1 = form.selectbox("Escolha um indicador de Contexto:",("População","Densidade demográfica","Grau de urbanização","População feminina","População preta/parda","Razão de dependência"),
+                              index=None,placeholder="Selecione um indicador...")
+    elif cat1 == "Renda":
+        ind1 = form.selectbox("Escolha um indicador de Renda:",("Índice Gini","Renda média da população","Renda da população feminina","Renda dos declarantes do IRPF"),
+                              index=None,placeholder="Selecione um indicador...")
+    elif cat1 == "Riqueza":
+        ind1 = form.selectbox("Escolha um indicador de Riqueza:",("Domicílios com bens duráveis","Número de veículos por pessoas","População declarante do IRPF","Patrimônio líquido médio da população","Patrimônio líquido médio dos declarantes do IRPF"),
+                              index=None,placeholder="Selecione um indicador...")
+
+    form.form_submit_button(label="Submit")
+
+
+
+
+
+with a2:
+    form = st.form(key="mapa2")
+
+    cat1 = form.selectbox("Escolha uma categoria:",("Contextualização","Renda","Riqueza"),key='mapa2',index=None,placeholder="Selecione uma categoria...")
+    #form.form_submit_button(label="Submit")
+
+#form = st.form(key="form_settings1")
+
+    if cat1 == "Contextualização":
+        ind1 = form.selectbox("Escolha um indicador de Contexto:",("População","Densidade demográfica","Grau de urbanização","População feminina","População preta/parda","Razão de dependência"),
+                              index=None,placeholder="Selecione um indicador...")
+    elif cat1 == "Renda":
+        ind1 = form.selectbox("Escolha um indicador de Renda:",("Índice de Gini","Renda média da população","Renda da população feminina","Renda dos declarantes do IRPF"),
+                              index=None,placeholder="Selecione um indicador...")
+    elif cat1 == "Riqueza":
+        ind1 = form.selectbox("Escolha um indicador de Riqueza:",("Domicílios com bens duráveis","Número de veículos por pessoas","População declarante do IRPF","Patrimônio líquido médio da população","Patrimônio líquido médio dos declarantes do IRPF"),
+                              index=None,placeholder="Selecione um indicador...")
+
+    form.form_submit_button(label="Submit")
+
+
+with a1:
+    if ind1 == 'População':
+        mapa('PR',contexto,'População','FisherJenks',5,'copper_r', ['Município','População'],'População (hab)')
+with a2:
+    if ind1 == 'Índice de Gini':
+        mapa('PR',renda,'Índice de Gini','FisherJenks',3,'PuBuGn', ['Município','Índice de Gini'],'Índice de Gini')
+
+
+
+
+
+
+
+
