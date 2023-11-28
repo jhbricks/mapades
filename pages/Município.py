19f0c8afdd86393abe3d9b6ef7fee92ad9923c31
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-11-27 23:11:59 -0300

    a

--- pages/Município.py
+++ pages/Município.py
@@ -12,13 +12,39 @@ op = st.selectbox("Selecione um município:",mun,index=None,placeholder="Selecio
 #geojson_filename = "./dados/PR.geojson"
 #gdf_geojson = gpd.read_file(geojson_filename)
 
-selected_municipality = df_csv[df_csv['Município'] == op]
+s_mun = df_csv[df_csv['Município'] == op]
 
-selected_indicators = ['Grau de Urbanização (%)', 'Razão de Dependência (%)', 'Densidade Demográfica (hab/km²)',
-                        'População feminina (%)', 'População preta ou parda (%)']
 
-fig = px.bar(selected_municipality, x='Município', y=selected_indicators, title=f'Indicators for {op}')
+
+s_ind = ['População residente','Grau de Urbanização (%)', 'Razão de Dependência (%)', 'Densidade Demográfica (hab/km²)','População feminina (%)', 'População preta ou parda (%)']
+
+
+
+fig = px.bar(s_mun, x='Município', y=s_ind, title=f'Indicators for {op}')
 st.plotly_chart(fig)
 
+
+import plotly.graph_objects as go
+
+s_mun = df_csv[df_csv['Município'] == op]
+
+fig = go.Figure()
+
+fig.add_trace(go.Bar(
+    x=s_mun,
+    y=s_ind,
+    name='Primary Product',
+    marker_color='indianred'
+))
+fig.add_trace(go.Bar(
+    x=s_mun,
+    y=s_ind,
+    name='Secondary Product',
+    marker_color='lightsalmon'
+))
+
+# Here we modify the tickangle of the xaxis, resulting in rotated labels.
+fig.update_layout(barmode='group', xaxis_tickangle=-45)
+st.plotly_chart(fig)
 
 
