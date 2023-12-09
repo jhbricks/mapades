b423af414b0d8347ec546bd4b8327374863cf73a
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-12-09 14:04:42 -0300

    up

--- pages/teste.py
+++ pages/teste.py
@@ -2,23 +2,10 @@ import leafmap.leafmap as leafmap
 import geopandas as gpd
 import pandas as pd
 import plotly.express as px
+import streamlit as st
+from PIL import Image
 
-arq_g = "./dados/geojson/PR.geojson"
-arq = "./dados/csv/contexto.csv"
+a = "./dados/imagem/icon/contx.gif"
 
-arq_csv = pd.read_csv(arq)
-arq_geojson = gpd.read_file(arq_g)
-data = arq_geojson.merge(arq_csv, on="Município")
-
-ind = "População"
-
-max_value = data[ind].max()
-min_value = data[ind].min()
-max_municipio = data.loc[data[ind] == max_value, "Município"].iloc[0]
-min_municipio = data.loc[data[ind] == min_value, "Município"].iloc[0]
-
-media = data[ind].mean().round(2)
-
-data_canada = px.data.gapminder().query("country == 'Canada'")
-fig = px.bar(data_canada, x='year', y='pop')
-fig.show()
+img = Image.open(a)
+st.button(st.image(img))
