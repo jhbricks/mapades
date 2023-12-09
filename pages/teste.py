dada8af03776b5679066cf04a7446b71d7b77063
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-12-09 14:47:18 -0300

    up

--- pages/teste.py
+++ pages/teste.py
@@ -5,7 +5,7 @@ import plotly.express as px
 import streamlit as st
 from PIL import Image
 
-a = "./dados/imagem/icon/contx.gif"
+a = "./dados/imagem/icon/renda.png"
 
 img = Image.open(a)
 st.button(st.image(img))
