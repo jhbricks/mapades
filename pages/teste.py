b56a2972bbaa38f4b4dd62933bd9940296312e43
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-12-09 15:49:15 -0300

    up

--- pages/teste.py
+++ pages/teste.py
@@ -12,13 +12,13 @@ img = Image.open(a)
 
 
 
-import streamlit as st
+#import streamlit as st
 
 # Carregue a imagem que você deseja usar como botão
-imagem_botao = "./dados/imagem/icon/renda.png"
+#imagem_botao = "./dados/imagem/icon/renda.png"
 
 # Exiba a imagem como uma imagem normal
-st.image(imagem_botao, use_column_width=True)
+#st.image(imagem_botao, use_column_width=True)
 
 # Adicione um link para outra página quando a imagem for clicada
 link_para_outra_pagina = "https://mapadesigualdade.streamlit.app/Renda"
