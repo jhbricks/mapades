8825c74ad8c77278e0daf0c6be9fa7ffb4b3e160
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-12-09 14:56:02 -0300

    a

--- pages/teste.py
+++ pages/teste.py
@@ -9,3 +9,18 @@ a = "./dados/imagem/icon/renda.png"
 
 img = Image.open(a)
 st.button(st.image(img))
+
+button_style = f"""
+                        <style>
+                        div.stButton > button {{
+                            background: url(data:a;base64,{encoded_image}) no-repeat;
+                            background-size: cover;
+                            background-position: center;
+                            height: 16em;
+                            width: 24em;
+                            box-shadow: 10px 10px 5px grey;
+                            margin: 15px;
+                        }}
+                        </style>
+                        """
+st.markdown(button_style, unsafe_allow_html=True)
