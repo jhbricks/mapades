18a79da60098fcef6d12e01db8bdbf5df5f5762d
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-12-10 05:39:42 -0300

    up

--- Início.py
+++ Início.py
@@ -1,100 +1,65 @@
 import streamlit as st
-from streamlit_extras.switch_page_button import switch_page
+import base64
+from PIL import Image
 
 
 st.set_page_config(layout="wide",page_title="Mapa da Desigualdade")
 
 #Remove os espaços em branco no topo
-st.markdown("""<style> .block-container {padding-top: 1rem;}</style> """, unsafe_allow_html=True)
-
-st.write("# Mapa da Desigualdade")
+st.markdown("""<style> .block-container {padding-top: 3rem;padding-bottom: 1rem;}</style> """, unsafe_allow_html=True)
+# Exibir o título centralizado
+texto_centralizado = """<div style="display: flex; justify-content: center; align-items: center"><h1>Mapa da Desigualdade</h1></div>"""
+st.write(texto_centralizado, unsafe_allow_html=True)
 
 #st.sidebar.success("Selecione uma das páginas acima")
 
 st.markdown("""*Mapa da Desigualdade é uma ferramenta que apresenta indicadores relacionados à
             diversos fatores que influenciam a qualidade de vida da população, destacando as
-            diferenças entre as regiões do Estado do Paraná e do Núcleo Territorial Central de Curitiba.*""")   
-st.markdown("**Selecione um dos temas abaixo ou abra o menu ao lado**")
+            diferenças entre as regiões do Estado do Paraná e do Núcleo Territorial Central de Curitiba.    
+            O Mapa da Desigualdade está em construção, por enquanto apenas as categorias Contextualização,
+            Renda e Riqueza estão com indicadores.*""")   
 
 
-#####Estilo dos botões
-m = st.markdown("""
-<style>
-div.stButton > button:first-child {
-    background-color: #fad3ac;
-    color: black;
-    height: 3em;
-    width: 10em;
-    border-radius:10px;
-    border:3px solid #fad3ac;
-    font-size:20px;
-    font-weight: bold;
-    margin: auto;
-    display: block;
-}
-
-div.stButton > button:hover {
-	background:linear-gradient(to bottom, #fad3ac 5%, #ffe8d1 100%);
-	background-color:#fad3ac;
-}
-</style>""", unsafe_allow_html=True)        
-
-#####Botões
-c1, c2, c3 = st.columns(3)
-c = 'st.sidebar.image(logos)'
-
-with c1:
-    aju = st.button("Ajuda")
-    if aju:
-        switch_page("Ajuda")
-        
-    cont = st.button("Contextualização")
-    if cont:
-        switch_page("Contextualização")
-
-    renda = st.button("Renda")
-    if renda:
-        switch_page("Renda")
-
-    riqz = st.button("Riqueza")
-    if riqz:
-        switch_page("Riqueza")
-
-with c2:
-    seg = st.button("Segurança")
-    if seg:
-        switch_page("Segurança")
-
-    amb = st.button("Meio ambiente")
-    if amb:
-        switch_page("Meio ambiente")
-
-    edu = st.button("Educação")
-    if edu:
-        switch_page("Educação")
-
-    mais = st.button("Mais temas")
-    if mais:
-        switch_page("Mais temas")
-
-with c3:
-    comp = st.button("Comparar")
-    if comp:
-        switch_page("Comparação")
-
-    mun = st.button("Município")
-    if mun:
-        switch_page("Município")
-
-    cla = st.button("Classificar")
-    if cla:
-        switch_page("Classificar")
-
-    sob = st.button("Sobre")
-    if sob:
-        switch_page("Sobre")
+texto = """<div style="display: flex; justify-content: center; align-items: center"><h1 style="font-size: 20px;"><b>Selecione um dos temas abaixo ou abra o menu ao lado</b></h1></div>"""
+st.write(texto, unsafe_allow_html=True)
 
 
+def criar_botao(link, imagem, texto, largura=150):
+    img_base64 = base64.b64encode(open(imagem, "rb").read()).decode()
+    return f'<a href="{link}" target="_self"><img src="data:image/png;base64,{img_base64}" width="{largura}" alt="{texto}"></a>'
+
+botoes = [
+    ("https://mapadesigualdade.streamlit.app/Contextualização", "./dados/imagem/icon/contx.png", "Contextualização"),
+    ("https://mapadesigualdade.streamlit.app/Segurança", "./dados/imagem/icon/seg.png", "Segurança"),
+    ("https://mapadesigualdade.streamlit.app/Mobilidade", "./dados/imagem/icon/mob.png", "Mobilidade"),
+    ("https://mapadesigualdade.streamlit.app/Mais_temas", "./dados/imagem/icon/mais.png", "Mais Temas"),
+    ("https://mapadesigualdade.streamlit.app/Ajuda", "./dados/imagem/icon/aju.png", "Ajuda"),
+    ("https://mapadesigualdade.streamlit.app/Renda", "./dados/imagem/icon/renda.png", "Renda"),
+    ("https://mapadesigualdade.streamlit.app/Meio_Ambiente", "./dados/imagem/icon/amb.png", "Meio Ambiente"),
+    ("https://mapadesigualdade.streamlit.app/Educação", "./dados/imagem/icon/edu.png", "Educação"),
+    ("https://mapadesigualdade.streamlit.app/Comparar", "./dados/imagem/icon/comp.png", "Comparar"),
+    ("https://mapadesigualdade.streamlit.app/Classificar", "./dados/imagem/icon/class.png", "Classificar"),
+    ("https://mapadesigualdade.streamlit.app/Riqueza", "./dados/imagem/icon/riq.png", "Riqueza"),
+    ("https://mapadesigualdade.streamlit.app/Saúde", "./dados/imagem/icon/saude.png", "Saúde"),
+    ("https://mapadesigualdade.streamlit.app/Cultura", "./dados/imagem/icon/cult.png", "Cultura"),
+    ("https://mapadesigualdade.streamlit.app/Município", "./dados/imagem/icon/mun.png", "Comparar"),
+    ("https://mapadesigualdade.streamlit.app/Sobre", "./dados/imagem/icon/sobre.png", "Sobre"),
+]
+
+num_colunas = 5
+num_linhas = 3
+
+espacamento_colunas = 10  # Espaço entre colunas
+
+# Criar a estrutura em HTML com os botões
+html_colunas = ""
+for linha in range(num_linhas):
+    html_colunas += f'<div style="display: flex; flex-direction: row; gap: 10px; justify-content: center; margin-bottom: {espacamento_colunas}px;">'
+    html_colunas += " ".join([criar_botao(*botoes[linha * num_colunas + i]) for i in range(num_colunas)])
+    html_colunas += '</div>'
+
+# Exibir a estrutura HTML
+st.write(html_colunas, unsafe_allow_html=True)
 
 
 #sidebar
@@ -107,13 +72,35 @@ st.markdown("""<style>[data-testid=stSidebar] [data-testid=stImage]{
             margin-right: auto;
             width: 80%;}</style>""", unsafe_allow_html=True)
 
-st.sidebar.info(
-            """**Realização**
-            \n [Kurytiba Metropole](https://www.kurytibametropole.org/) 
-            \n [Gemaeco](https://gemaeco.ufpr.br/)
-            \n [PPGCG](https://cienciasgeodesicas.ufpr.br/)
-            \n [Mais informações](https://mapadesigualdade.streamlit.app/Sobre)""")
 with st.sidebar:
     st.sidebar.image(logos)
 
-       
+st.write("##")
+st.write("##")
+
+i4 = "./dados/imagem/realiz.png"
+img = base64.b64encode(open(i4, "rb").read()).decode()
+link = "https://mapadesigualdade.streamlit.app/Sobre"
+
+# Adicionando o estilo CSS para centralizar a imagem
+estilo_css = """
+<style>
+    .centralizar-imagem {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+    }
+</style>
+"""
+
+# Adicionando o estilo ao Streamlit
+st.markdown(estilo_css, unsafe_allow_html=True)
+
+# Exibindo a imagem com a classe CSS
+st.markdown(f"""<a href="{link}" target="_self"><img class="centralizar-imagem" src="data:image/png;base64,{img}" width="500"></a>""",unsafe_allow_html=True)
+
+
+
+
+
+
