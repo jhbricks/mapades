d8184893a647c87ef31b83354666854e416f65f9
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-12-10 03:06:35 -0300

    up

---
+++ pages/Cultura.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from streamlit_extras.colored_header import colored_header
+
+st.set_page_config(layout="wide", page_title="Cultura - Mapa da Desigualdade")
+
+colored_header(label=":violet[Indicadores de Cultura: em breve ⌛]",
+               description="   ",
+               color_name="violet-70",)
