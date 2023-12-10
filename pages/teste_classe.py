ea6c52fc2001539127bee8d9228577b4d230b43f
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-12-10 18:18:36 -0300

    up

--- pages/teste_classe.py
+++ pages/teste_classe.py
@@ -30,24 +30,28 @@ def criar_botao(link, imagem, texto, largura=150):
 
 botoes = [
     ("https://mapadesigualdade.streamlit.app/Contextualização", "./dados/imagem/icon/contx.png", "Contextualização"),
-    ("https://mapadesigualdade.streamlit.app/Segurança", "./dados/imagem/icon/seg.png", "Segurança"),
-    ("https://mapadesigualdade.streamlit.app/Mobilidade", "./dados/imagem/icon/mob.png", "Mobilidade"),
-    ("https://mapadesigualdade.streamlit.app/Mais_temas", "./dados/imagem/icon/mais.png", "Mais Temas"),
-    ("https://mapadesigualdade.streamlit.app/Ajuda", "./dados/imagem/icon/aju.png", "Ajuda"),
     ("https://mapadesigualdade.streamlit.app/Renda", "./dados/imagem/icon/renda.png", "Renda"),
-    ("https://mapadesigualdade.streamlit.app/Meio_Ambiente", "./dados/imagem/icon/amb.png", "Meio Ambiente"),
-    ("https://mapadesigualdade.streamlit.app/Educação", "./dados/imagem/icon/edu.png", "Educação"),
-    ("https://mapadesigualdade.streamlit.app/Comparar", "./dados/imagem/icon/comp.png", "Comparar"),
-    ("https://mapadesigualdade.streamlit.app/Classificar", "./dados/imagem/icon/class.png", "Classificar"),
     ("https://mapadesigualdade.streamlit.app/Riqueza", "./dados/imagem/icon/riq.png", "Riqueza"),
+    
+    ("https://mapadesigualdade.streamlit.app/Segurança", "./dados/imagem/icon/seg.png", "Segurança"),
+    ("https://mapadesigualdade.streamlit.app/Meio_Ambiente", "./dados/imagem/icon/amb.png", "Meio Ambiente"),
     ("https://mapadesigualdade.streamlit.app/Saúde", "./dados/imagem/icon/saude.png", "Saúde"),
+    
+    ("https://mapadesigualdade.streamlit.app/Mobilidade", "./dados/imagem/icon/mob.png", "Mobilidade"),
+    ("https://mapadesigualdade.streamlit.app/Educação", "./dados/imagem/icon/edu.png", "Educação"),
     ("https://mapadesigualdade.streamlit.app/Cultura", "./dados/imagem/icon/cult.png", "Cultura"),
-    ("https://mapadesigualdade.streamlit.app/Município", "./dados/imagem/icon/mun.png", "Comparar"),
-    ("https://mapadesigualdade.streamlit.app/Sobre", "./dados/imagem/icon/sobre.png", "Sobre"),
+
+    ("https://mapadesigualdade.streamlit.app/Mais_temas", "./dados/imagem/icon/mais.png", "Mais Temas"),
+    ("https://mapadesigualdade.streamlit.app/Comparar_indicadores", "./dados/imagem/icon/comp.png", "Comparar"),
+    ("https://mapadesigualdade.streamlit.app/Município", "./dados/imagem/icon/mun.png", "Município"),
+
+    ("https://mapadesigualdade.streamlit.app/Ajuda", "./dados/imagem/icon/aju.png", "Ajuda"),
+    ("https://mapadesigualdade.streamlit.app/Classificar", "./dados/imagem/icon/class.png", "Classificar"),
+        ("https://mapadesigualdade.streamlit.app/Sobre", "./dados/imagem/icon/sobre.png", "Sobre"),
 ]
 
-num_colunas = 5
-num_linhas = 3
+num_colunas = 3
+num_linhas = 5
 
 espacamento_colunas = 10  # Espaço entre colunas
 
@@ -109,7 +113,7 @@ estilo_css = """
 st.markdown(estilo_css, unsafe_allow_html=True)
 
 # Exibindo a imagem com a classe CSS
-st.markdown(f"""<a href="{link}" target="_self"><img class="centralizar-imagem" src="data:image/png;base64,{img}" width="500"></a>""",unsafe_allow_html=True)
+st.markdown(f"""<a href="{link}" target="_self"><img class="centralizar-imagem" src="data:image/png;base64,{img}" width="400"></a>""",unsafe_allow_html=True)
 
 
 
