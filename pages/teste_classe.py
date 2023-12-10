d65946baa0b4350ea0f0625f185beb77474760ff
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-12-10 18:21:16 -0300

    up

--- pages/teste_classe.py
+++ pages/teste_classe.py
@@ -20,7 +20,7 @@ st.markdown("""*Mapa da Desigualdade é uma ferramenta que apresenta indicadores
             Renda e Riqueza estão com indicadores.*""")   
 
 
-texto = """<div style="display: flex; justify-content: center; align-items: center"><h1 style="font-size: 20px;"><b>Selecione um dos temas abaixo ou abra o menu ao lado</b></h1></div>"""
+texto = """<div style="display: flex; justify-content: center; align-items: center"><h1 style="font-size: 15px;"><b>Selecione um dos temas abaixo ou abra o menu ao lado:</b></h1></div>"""
 st.write(texto, unsafe_allow_html=True)
 
 
