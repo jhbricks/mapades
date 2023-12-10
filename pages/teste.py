230f4223e05f9e0707c0bf93771bca7343214ff2
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-12-10 03:02:50 -0300

    up

--- pages/teste.py
+++ pages/teste.py
@@ -57,14 +57,14 @@ with d:
 with f:
 # Use st.markdown para exibir a imagem como um link clicável
     img8 = "./dados/imagem/icon/mob.png"
-    img_b8 = base64.b64encode(open(img1, "rb").read()).decode()
+    img_b8 = base64.b64encode(open(img8, "rb").read()).decode()
     link4 = "https://mapadesigualdade.streamlit.app/Mobilidade"
     st.markdown(f"""<a href="{link4}" target="_self"><img src="data:image/png;base64,{img_b4}" width="150"></a>""",unsafe_allow_html=True)
     
-    img5 = "./dados/imagem/icon/edu.png"
-    img_b5 = base64.b64encode(open(img5, "rb").read()).decode()
-    link5 = "https://mapadesigualdade.streamlit.app/Educação"
-    st.markdown(f"""<a href="{link5}" target="_self"><img src="data:image/png;base64,{img_b5}" width="150"></a>""",unsafe_allow_html=True)
+    img9 = "./dados/imagem/icon/edu.png"
+    img_b9 = base64.b64encode(open(img9, "rb").read()).decode()
+    link9 = "https://mapadesigualdade.streamlit.app/Educação"
+    st.markdown(f"""<a href="{link9}" target="_self"><img src="data:image/png;base64,{img_b5}" width="150"></a>""",unsafe_allow_html=True)
     
     img7 = "./dados/imagem/icon/cult.png"
     img_b7 = base64.b64encode(open(img7, "rb").read()).decode()
@@ -77,28 +77,28 @@ with g:
     link0 = "https://mapadesigualdade.streamlit.app/Mais_temas"
     st.markdown(f"""<a href="{link0}" target="_self"><img src="data:image/png;base64,{img_b0}" width="150"></a>""",unsafe_allow_html=True)
 
-    img7 = "./dados/imagem/icon/comp.png"
-    img_b7 = base64.b64encode(open(img7, "rb").read()).decode()
-    link7 = "https://mapadesigualdade.streamlit.app/Comparar"
-    st.markdown(f"""<a href="{link7}" target="_self"><img src="data:image/png;base64,{img_b7}" width="150"></a>""",unsafe_allow_html=True)
+    img10 = "./dados/imagem/icon/comp.png"
+    img_b10 = base64.b64encode(open(img10, "rb").read()).decode()
+    link10 = "https://mapadesigualdade.streamlit.app/Comparar"
+    st.markdown(f"""<a href="{link10}" target="_self"><img src="data:image/png;base64,{img_b7}" width="150"></a>""",unsafe_allow_html=True)
 
-    img6 = "./dados/imagem/icon/mun.png"
-    img_b6 = base64.b64encode(open(img1, "rb").read()).decode()
-    link6 = "https://mapadesigualdade.streamlit.app/Município"
-    st.markdown(f"""<a href="{link6}" target="_self"><img src="data:image/png;base64,{img_b6}" width="150"></a>""",unsafe_allow_html=True)
+    img11 = "./dados/imagem/icon/mun.png"
+    img_b11 = base64.b64encode(open(img11, "rb").read()).decode()
+    link11 = "https://mapadesigualdade.streamlit.app/Município"
+    st.markdown(f"""<a href="{link11}" target="_self"><img src="data:image/png;base64,{img_b6}" width="150"></a>""",unsafe_allow_html=True)
 
 with h:
-    img0 = "./dados/imagem/icon/aju.png"
-    img_b0 = base64.b64encode(open(img0, "rb").read()).decode()
-    link0 = "https://mapadesigualdade.streamlit.app/Ajuda"
-    st.markdown(f"""<a href="{link0}" target="_self"><img src="data:image/png;base64,{img_b0}" width="150"></a>""",unsafe_allow_html=True)
+    img12 = "./dados/imagem/icon/aju.png"
+    img_b12 = base64.b64encode(open(img12, "rb").read()).decode()
+    link12 = "https://mapadesigualdade.streamlit.app/Ajuda"
+    st.markdown(f"""<a href="{link12}" target="_self"><img src="data:image/png;base64,{img_b0}" width="150"></a>""",unsafe_allow_html=True)
 
-    img7 = "./dados/imagem/icon/class.png"
-    img_b7 = base64.b64encode(open(img7, "rb").read()).decode()
-    link7 = "https://mapadesigualdade.streamlit.app/Classificar"
-    st.markdown(f"""<a href="{link7}" target="_self"><img src="data:image/png;base64,{img_b7}" width="150"></a>""",unsafe_allow_html=True)
+    img13 = "./dados/imagem/icon/class.png"
+    img_b13 = base64.b64encode(open(img13, "rb").read()).decode()
+    link13 = "https://mapadesigualdade.streamlit.app/Classificar"
+    st.markdown(f"""<a href="{link13}" target="_self"><img src="data:image/png;base64,{img_b7}" width="150"></a>""",unsafe_allow_html=True)
 
-    img6 = "./dados/imagem/icon/sobre.png"
-    img_b6 = base64.b64encode(open(img1, "rb").read()).decode()
-    link6 = "https://mapadesigualdade.streamlit.app/Sobre"
-    st.markdown(f"""<a href="{link6}" target="_self"><img src="data:image/png;base64,{img_b6}" width="150"></a>""",unsafe_allow_html=True)
+    img14 = "./dados/imagem/icon/sobre.png"
+    img_b14 = base64.b64encode(open(img14, "rb").read()).decode()
+    link14 = "https://mapadesigualdade.streamlit.app/Sobre"
+    st.markdown(f"""<a href="{link14}" target="_self"><img src="data:image/png;base64,{img_b6}" width="150"></a>""",unsafe_allow_html=True)
