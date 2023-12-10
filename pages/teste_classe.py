4f279adec0ba1590ea954ae257e24c0a12c90dd0
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-12-10 19:02:13 -0300

    up

--- pages/teste_classe.py
+++ pages/teste_classe.py
@@ -118,6 +118,19 @@ st.markdown(f"""<a href="{link}" target="_self"><img class="centralizar-imagem" 
 
 
 
+from streamlit_option_menu import option_menu
+import streamlit.components.v1 as html
 
+with st.sidebar:
+    choose = option_menu("Mapa da Desigualdade", ["Início", "Contextualização", "Renda", "Riqueza", "Segurança"],
+                         icons=['house', 'camera fill', 'kanban', 'book','person lines fill'],
+                         menu_icon="app-indicator", default_index=0,
+                         styles={
+        "container": {"padding": "5!important", "background-color": "#fafafa"},
+        "icon": {"color": "orange", "font-size": "25px"}, 
+        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "#02ab21"},
+    }
+    )
 
 
