44a8c7377cb7f87217227436edecef3cd576f036
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-12-10 04:03:45 -0300

    up

--- pages/teste_classe.py
+++ pages/teste_classe.py
@@ -31,10 +31,9 @@ espacamento_colunas = 10  # Ajuste conforme necessário
 # Criar a estrutura em HTML com os botões
 html_colunas = ""
 for linha in range(num_linhas):
-    html_colunas += '<div style="display: flex; flex-direction: row; gap: 20px; justify-content: center;">'
+    html_colunas += f'<div style="display: flex; flex-direction: row; gap: 10px; justify-content: center; margin-bottom: {espacamento_colunas}px;">'
     html_colunas += " ".join([criar_botao(*botoes[linha * num_colunas + i]) for i in range(num_colunas)])
     html_colunas += '</div>'
-    html_colunas += '<hr style="margin: 10px;">'  # Adicionar espaçamento entre linhas
 
 # Exibir a estrutura HTML
 st.write(html_colunas, unsafe_allow_html=True)
