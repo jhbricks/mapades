40545751f9e3dd0e3599132af57ca7e03ca092a8
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-12-10 03:43:57 -0300

    up

--- pages/teste_classe.py
+++ pages/teste_classe.py
@@ -14,12 +14,12 @@ botoes = [
     ("https://mapadesigualdade.streamlit.app/Renda", "./dados/imagem/icon/renda.png", "Renda"),
     ("https://mapadesigualdade.streamlit.app/Meio_Ambiente", "./dados/imagem/icon/amb.png", "Meio Ambiente"),
     ("https://mapadesigualdade.streamlit.app/Educação", "./dados/imagem/icon/edu.png", "Educação"),
-    ("https://mapadesigualdade.streamlit.app/Município", "./dados/imagem/icon/mun.png", "Município"),
+    ("https://mapadesigualdade.streamlit.app/Comparar", "./dados/imagem/icon/comp.png", "Comparar"),
     ("https://mapadesigualdade.streamlit.app/Classificar", "./dados/imagem/icon/class.png", "Classificar"),
     ("https://mapadesigualdade.streamlit.app/Riqueza", "./dados/imagem/icon/riq.png", "Riqueza"),
     ("https://mapadesigualdade.streamlit.app/Saúde", "./dados/imagem/icon/saude.png", "Saúde"),
     ("https://mapadesigualdade.streamlit.app/Cultura", "./dados/imagem/icon/cult.png", "Cultura"),
-    ("https://mapadesigualdade.streamlit.app/Comparar", "./dados/imagem/icon/comp.png", "Comparar"),
+    ("https://mapadesigualdade.streamlit.app/Município", "./dados/imagem/icon/mun.png", "Município"),
     ("https://mapadesigualdade.streamlit.app/Sobre", "./dados/imagem/icon/sobre.png", "Sobre"),
 ]
 
