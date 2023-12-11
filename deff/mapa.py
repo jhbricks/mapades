80eea2a8741bfba4bdbe98d910c8bc2ade21664e
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-12-10 23:03:10 -0300

    up

--- deff/mapa.py
+++ deff/mapa.py
@@ -43,9 +43,9 @@ def mapa (area,arq,ind,scheme,k,cmap,fields,title):
 
 #######LAT E LON CENTRAIS
   ponto_central = arq_geojson.geometry.centroid
-  lat = ponto_central.iloc[0].y
+  abc = ponto_central.iloc[0].y
   lon = ponto_central.iloc[0].x
-
+  lat = abc + 0.5
   st.markdown(f"<p><font size='+7' color='darkpurple'> lat ={lat} long = {lon}</font></p>", unsafe_allow_html=True)
     
   if not isinstance(data,gpd.GeoDataFrame):
